82ae14bafe9d347edddf79d491d008a2259b5e5a
Raskolnikov <jerry.tann@endress.com>
2022-04-30 18:42:06 +0800

    ✨ start the temparature problem

---
+++ 03temperatures/temp.rs
@@ -0,0 +1,30 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let n = parse_input!(input_line, i32); // the number of temperatures to analyse
+    let mut inputs = String::new();
+    io::stdin().read_line(&mut inputs).unwrap();
+    let mut min:i32 = 100000;
+    for i in inputs.split_whitespace() {
+        let t = parse_input!(i, i32);
+        let abs = t.abs();
+        let minabs = min.abs();
+        if abs < minabs {
+            min = t;
+        }
+    }
+
+    // Write an answer using println!("message...");
+    // To debug: eprintln!("Debug message...");
+    println!("{}", min)
+}
