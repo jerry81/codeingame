66c56fa8a46ce65f6cb1d85a31ca7cfde2cb2a3d
Raskolnikov <jerry.tann@endress.com>
2022-05-01 09:53:05 +0800

    ✨ ascii problem starting code

---
+++ 05AsciiArt/ascii.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let l = parse_input!(input_line, i32);
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let h = parse_input!(input_line, i32);
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let t = input_line.trim_matches('\n').to_string();
+    
+    for i in 0..h as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let row = input_line.trim_matches('\n').to_string();
+    }
+
+    // Write an answer using println!("message...");
+    // To debug: eprintln!("Debug message...");
+
+    println!("answer");
+}
