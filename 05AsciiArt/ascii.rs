70d5a01c2ae76588b16a4c6ac16f57302555adf2
Raskolnikov <jerry.tann@endress.com>
2022-05-01 19:10:39 +0800

    ✨ almost there, missing one case

--- 05AsciiArt/ascii.rs
+++ 05AsciiArt/ascii.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::collections::HashMap;
 
 macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
@@ -11,67 +12,51 @@ macro_rules! parse_input {
 fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
-    let l = parse_input!(input_line, i32);
+    let n = parse_input!(input_line, i32); // Number of elements which make up the association table.
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
-    let h = parse_input!(input_line, i32);
-    let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).unwrap();
-    let t = input_line.trim_matches('\n').to_string();
-    let mut lookup = [(); 30].map(|_| String::new());
-    let mut lookupslices = [(); 30].map(|_| "");
-    for i in 0..h as usize {
+    let q = parse_input!(input_line, i32); // Number Q of file names to be analyzed.
+    let mut lookup: HashMap<String, String> = HashMap::new();
+    for i in 0..n as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
-        let row = input_line.trim_matches('\n').to_string();
-        lookup[i] = row;
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let ext = inputs[0].trim().to_string(); // file extension
+        let mt = inputs[1].trim().to_string(); // MIME type.
+        lookup.insert(ext, mt);
     }
-    for i in 0..h as usize {
-        lookupslices[i] = &lookup[i].as_str()
+    let mut answers = [(); 10000].map(|_| String::new());
+    for i in 0..q as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let fname = input_line.trim_matches('\n').to_string(); // One file name per line.
+        let iter = fname.split(".");
+        let mut ext = "";
+        let mut flag = true;
+        for i in iter {
+         if flag {
+             flag = false;
+             continue;
+         }
+         ext = i;
+        }
+        
+        eprintln!("fname is {}", ext);
+        if lookup.contains_key(ext) {
+          eprintln!("answer {}", lookup[ext].to_string());
+          answers[i] = lookup[ext].to_string();
+        } else {
+            eprintln!("UNKNOWN");
+            answers[i] = "UNKNOWN".to_string();
+        }
     }
-    let mut answers = [(); 30].map(|_| String::new());
-    let mut index: u32 = 0;
-    eprintln!("height is {}", h);
-    for c in t.bytes() { 
-        // do something with `c`
-        // convert byte to position
-        // 65 - 90 A-Z
-        // 97 - 122 a-z 
-        let mut startIndex = 0;
-        let mut endIndex = l;
-        if c >= 65 && c <= 90 {
-          // slice out [((c - 65) * l), (c-64)*l] 
-          startIndex = (c as i32 - 65) * l;
-          endIndex = startIndex + l;
-          // todo: ugly
-          for i in 0..h as usize {
-            let slice = &lookupslices[i][startIndex as usize..(endIndex) as usize];
-            answers[i] += slice;
-          }
-          continue;
-        }
-        if c >= 97 && c <= 122 {
-         // slice out [((c - 97) * l), (c-96)*l] 
-          startIndex = (c as i32 - 97) * l;
-          endIndex = startIndex + l;
-          // todo: ugly 
-          for i in 0..h as usize {
-            let slice = &lookupslices[i][startIndex as usize..(endIndex) as usize];
-            answers[i] += slice;
-          }
-          continue;
-        }
-        for i in 0..h as usize {
-            let slice = &lookupslices[i][(26*l) as usize..];
-            answers[i] += slice;
-        }
-        // use the ?
-        // index, 27 * l
+    for i in 0..q {
+        println!("{}", answers[i as usize])
     }
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
-    for i in 0..h as usize {
-        println!("{}", answers[i]);
-    }
-    
+
+
+    // For each of the Q filenames, display on a line the corresponding MIME type. If there is no corresponding type, then display UNKNOWN.
+    // println!("UNKNOWN");
 }
