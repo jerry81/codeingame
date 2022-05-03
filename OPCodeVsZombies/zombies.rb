ed79cbd94f82dd711126811ec5d08f8b05e7203d
Raskolnikov <jerry.tann@endress.com>
2022-05-03 14:46:23 +0800

    ✨ pass first testcase

--- OPCodeVsZombies/zombies.rb
+++ OPCodeVsZombies/zombies.rb
@@ -10,18 +10,37 @@ loop do
   zombies = []
   human_count.times do
     human_id, human_x, human_y = gets.split(" ").collect { |x| x.to_i }
-    humans << { id => human_id, x => human_x, y => human_y }
+    humans << { :id => human_id, :x => human_x, :y => human_y }
   end
   zombie_count = gets.to_i
   zombie_count.times do
     zombie_id, zombie_x, zombie_y, zombie_xnext, zombie_ynext = gets.split(" ").collect { |x| x.to_i }
-    zombies << { id => zombie_id, x => zombie_x, y => zombie_y }
+    zombies << { :id => zombie_id, :x => zombie_x, :y => zombie_y }
   end
-
-  # find closest zombie to human 
+  STDERR.puts "humans #{humans} and zombies #{zombies}"
+  # find closest zombie to human
+  closestD = nil
+  closestMidX = nil
+  closestMidY = nil
+  zombies.each do |z|
+    humans.each do |h|
+      dx = (h[:x] - z[:x]).abs
+      dy = (h[:y] - z[:y]).abs
+      midx = (h[:x] + z[:x] / 2)
+      midy = (h[:y] + h[:y] / 2)
+      dist = Math.sqrt((dx ** 2) + (dy ** 2))
+      if closestD.nil? || closestD > dist
+        closestD = dist
+        closestMidX = midx
+        closestMidY = midy
+        closestD = dist
+        next
+      end
+    end
+  end
 
   # Write an action using puts
   # To debug: STDERR.puts "Debug messages..."
 
-  puts "0 0" # Your destination coordinates
+  puts "#{closestMidX} #{closestMidY}" # Your destination coordinates
 end
