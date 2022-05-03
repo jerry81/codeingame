259665e5b70b066cffc6886e3583b1156298da33
Raskolnikov <jerry.tann@endress.com>
2022-05-03 12:08:29 +0800

    ✨ first submission, getting dimensions mixed up

--- 11spoon1/spoon1.rb
+++ 11spoon1/spoon1.rb
@@ -4,14 +4,48 @@ STDOUT.sync = true # DO NOT REMOVE
 width = gets.to_i # the number of cells on the X axis
 height = gets.to_i # the number of cells on the Y axis
 
-grid = 
-height.times do
+grid = []
+nodes = []
+STDERR.puts "height #{height} width #{width}"
+for j in 0..height-1 do
   line = gets.chomp # width characters, each either 0 or .
+  grid[j] = []
+  for i in 0..width-1 do
+    STDERR.puts "loop #{line[i]}"
+    grid[j][i] = line[i]
+    if line[i] == '0'
+      nodes << { :x => i, :y => j }
+    end
+  end
 end
-
+STDERR.puts "i am out"
+STDERR.puts "grid #{grid} nodes #{nodes}"
 # Write an action using puts
 # To debug: STDERR.puts "Debug messages..."
 
 
 # Three coordinates: a node, its right neighbor, its bottom neighbor
-puts "0 0 1 0 0 1"
+
+nodes.each do | node |
+    x = node[:x]
+    y = node[:y]
+    rightX = x < width - 1 ? x+1 : -1
+    rightY = x < width - 1 ? y : -1
+    downX = y < height - 1 ? x : -1
+    downY = y < height - 1 ? y+1 : -1
+    if rightX > 0 
+        right = grid[rightX, rightY]
+        if right == '.'
+            rightX = -1
+            rightY = -1
+        end
+    end
+    if downY > 0
+        down = grid[rightX, rightY]
+        if down == '.'
+           downX = -1
+           downY = -1
+        end
+    end
+    puts "#{x} #{y} #{rightX} #{rightY} #{downX} #{downY}"
+end
