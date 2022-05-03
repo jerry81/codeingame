27f08d1d06f79206ef97efe10b1f1a9a6fbac2da
Raskolnikov <jerry.tann@endress.com>
2022-05-03 11:44:34 +0800

    ✨ initial code for no spoon part 1

---
+++ 11spoon1/spoon1.rb
@@ -0,0 +1,17 @@
+STDOUT.sync = true # DO NOT REMOVE
+# Don't let the machines win. You are humanity's last hope...
+
+width = gets.to_i # the number of cells on the X axis
+height = gets.to_i # the number of cells on the Y axis
+
+grid = 
+height.times do
+  line = gets.chomp # width characters, each either 0 or .
+end
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+
+# Three coordinates: a node, its right neighbor, its bottom neighbor
+puts "0 0 1 0 0 1"
