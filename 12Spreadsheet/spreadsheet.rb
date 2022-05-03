fadca429362b2bcc5f0576176bb9a04f73a8da4b
Raskolnikov <jerry.tann@endress.com>
2022-05-03 18:14:56 +0800

    ✨ trying to handle dependencies iteratively,  getting messy

--- 12Spreadsheet/spreadsheet.rb
+++ 12Spreadsheet/spreadsheet.rb
@@ -6,15 +6,21 @@ lines = []
 count = 0
 # tests did not pass because i did not consider backwards dependencies
 # we cannot resolve all dependencies with a single pass thru while reading in the lines for the first time.
-
-n.times do
-  operation, arg_1, arg_2 = gets.split(" ")
+dependencies = {}
+original = []
+def getValue(operation, arg_1, arg_2, lines, count, dependencies)
   asInt = 0
   asInt2 = 0
   if arg_1[0] == '$'
       rem = arg_1[1..]
       idx = rem.to_i
       asInt = lines[idx]
+      if asInt.nil?
+        if dependencies[count].nil?
+            dependencies[count] = []
+        end
+        dependencies[count] << idx 
+      end
   else 
      asInt = arg_1.to_i
   end
@@ -22,11 +28,18 @@ n.times do
     rem = arg_2[1..]
     idx = rem.to_i
     asInt2 = lines[idx]
+    if asInt2.nil?
+      dependencies << { :ct => count, :idx => idx }
+    end
   elsif arg_2[0] == '_'
     asInt2 = 0
   else 
     asInt2 = arg_2.to_i
   end
+  if (asInt.nil? || asInt2.nil?)
+    return nil
+  end
+
   case operation
   when "VALUE"
     lines[count] = asInt
@@ -37,13 +50,36 @@ n.times do
   when "MULT"
     lines[count] = asInt * asInt2
   end
+end
+
+n.times do
+  operation, arg_1, arg_2 = gets.split(" ")
+  original << [operation, arg_1, arg_2]
+  val = getValue(operation,arg_1,arg_2,lines,count,dependencies)
   count += 1
 end
+STDERR.puts " original #{original}"
+STDERR.puts " dep #{dependencies}"
+STDERR.puts " lines #{lines}"
+
+def recalcDependency(dependencies, lines, original)
+  dependencies.each do |k,v|
+    STDERR.puts "k #{k} v #{v}"
+    v.each do |d|
+        if lines[d]
+            oline = original[k]
+            newVal = getValue(oline[0], oline[1], oline[2], lines, k, dependencies)
+        end
+    end
+  end
+end
+
+recalcDependency(dependencies, lines, original)
+
 count = 0
 n.times do
   puts "#{lines[count]}"
   # Write an answer using puts
   # To debug: STDERR.puts "Debug messages..."
-  
   count += 1
 end
