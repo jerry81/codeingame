2f3edaaf20add14794d53d96e8a08d78bedf8adf
Raskolnikov <jerry.tann@endress.com>
2022-05-05 20:05:51 +0800

    ✨ try build dict

--- 14Circuits/circuits.cs
+++ 14Circuits/circuits.cs
@@ -14,13 +14,22 @@ class Solution
     static void Main(string[] args)
     {
         // make dict of circuits 
+        // Create a new dictionary of strings, with string keys.
+        Dictionary<string, int> resistance =
+          new Dictionary<string, int>();
+
+        // Add some elements to the dictionary. There are no
+         // duplicate keys, but some of the values are duplicates.
+        // resistance.Add("txt", "notepad.exe");
         int N = int.Parse(Console.ReadLine());
         for (int i = 0; i < N; i++)
         {
             string[] inputs = Console.ReadLine().Split(' ');
             string name = inputs[0];
             int R = int.Parse(inputs[1]);
+            resistance.Add(name, R);
         }
+        Console.Error.WriteLine("Debug messages...", resistance);
         string circuit = Console.ReadLine();
         // identify series and parallels
         // simple nesting example
