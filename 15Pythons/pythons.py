f76cfb6ba66d7f20f78b789b49762f6fe647550e
Raskolnikov <jerry.tann@endress.com>
2022-05-06 09:29:39 +0800

    ✨ test deleting from array

--- 15Pythons/pythons.py
+++ 15Pythons/pythons.py
@@ -6,15 +6,28 @@ import math
 
 
 strArr = []
+heads = []
 n, m = [int(i) for i in input().split()]
 for i in range(n):
     t = input()
     strArr.append(t)
+    for j in range(m):
+        if t[j] == 'o':
+            heads.append({i:i, j:j})
 
 print("strArr is ", strArr, file=sys.stderr, flush=True)
+print("heads is ", heads, file=sys.stderr, flush=True)
 
+# test removing a head
+
+item = heads[0]
+heads.remove(item)
+print("heads after delete ", heads, file=sys.stderr, flush=True)
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+
+
 
 print("Answer")
 
