c26f0d182f15bb280b14a3cd183785f1a79d43ef
Raskolnikov <jerry.tann@endress.com>
2022-05-07 14:51:22 +0800

    ✨ pretty print array to error, push to array

--- 16DFS1/dfs1.php
+++ 16DFS1/dfs1.php
@@ -13,17 +13,23 @@ error_log(var_export("expect 3 $N expect 2 $L expect 1 $E", true));
 /*
 first example 3 nodes
 */
+$edges = [];
 for ($i = 0; $i < $L; $i++)
 {
+
     // $N1: N1 and N2 defines a link between these nodes
     fscanf(STDIN, "%d %d", $N1, $N2);
     
 }
+$gateways = [];
 for ($i = 0; $i < $E; $i++)
 {
     // $EI: the index of a gateway node
     fscanf(STDIN, "%d", $EI);
+    array_push($gateways,$EI);
 }
+
+error_log(print_r($gateways, true));
 
 // game loop
 while (TRUE)
