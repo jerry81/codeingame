8284f2d92144f1a0e0a8794e0a03e75565a9137c
Raskolnikov <jerry.tann@endress.com>
2022-05-16 17:30:37 +0800

    ✨ second clash of code - failed!

---
+++ CoC/01coc.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+h = int(input())
+
+buckets = []
+sizes = []
+total_size = 0
+
+for i in range(h):
+    a,b = input().split()
+    a = int(a)
+    b = int(b[0:len(b)-1])
+    sizes.append(a)
+    total_size += a*b/100
+
+sizes.sort(reverse=True)
+
+for size in sizes:
+    if total_size > size:
+        buckets.append("100%")
+        total_size -= size
+    else:
+        proc = round(total_size/size*100)
+        buckets.append(str(proc)+"%")
+        break
+
+for i in range(len(buckets)):
+    print(str(sizes[i])+" "+buckets[i])
+
+
---
+++ CoC/README01.md
@@ -0,0 +1,17 @@
+# fill buckets
+
+input is number of buckets
+capacity of bucket {space} percent full
+
+output is sorted list of largest buckets filled up 
+
+## example 
+
+input is 
+2
+100 90%
+100 70%
+
+expected output is 
+100 100%
+100 60%
