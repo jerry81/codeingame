bfd80cea4900efd592e8cab8086a79fdb33fe128
Raskolnikov <jerry.tann@endress.com>
2022-05-22 14:48:40 +0800

    ✨ rock paper scissors initial

---
+++ 23RockPaperScissorsLizardSpock/rockpaper.rb
@@ -0,0 +1,13 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = gets.to_i
+n.times do
+  numplayer, signplayer = gets.split(" ")
+  numplayer = numplayer.to_i
+end
+
+# Write an answer using puts
+# To debug: STDERR.puts "Debug messages..."
+
+puts "WHO IS THE WINNER?"
