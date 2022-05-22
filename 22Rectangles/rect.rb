133f09430d1c0c9142ba456bd909baaf068d8201
Raskolnikov <jerry.tann@endress.com>
2022-05-22 14:15:40 +0800

    ✨ use memo but still too slow

--- 22Rectangles/rect.rb
+++ 22Rectangles/rect.rb
@@ -23,19 +23,37 @@ end
 STDERR.puts "points are #{points}"
 STDERR.puts "12 is #{points.size}"
 sq_count = 0 
+memox = {}
+memoy = {}
 points.each_with_index do |po,i|
   x1 = po[:x] # p seems to be reserved? - yes p is a method like "puts"
   y1 = po[:y]
+  if memox[x1].nil? 
+    memox[x1] = {}
+  end
+
+  if memoy[y1].nil? 
+    memoy[y1] = {}
+  end
+
   sl = points[i..]
   sl.each do |q|
     x2 = q[:x]
     y2 = q[:y]
+    
     if (x2 == x1) || (y2 == y1)
       next 
     end
 
+    if memox[x1][x2].nil?
+      memox[x1][x2] = x2-x1
+    end
+
+    if memoy[y1][y2].nil?
+      memoy[y1][y2] = y2-y1 
+    end
     # now check if it is a square 
-    if (x2 - x1) == (y2 - y1) 
+    if (memox[x1][x2]) == (memoy[y1][y2]) 
       sq_count += 1
     end
   end
