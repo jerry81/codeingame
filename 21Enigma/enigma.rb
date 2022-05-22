6fef4689d6dd94362271313d56377d96c6c10fb2
Raskolnikov <jerry.tann@endress.com>
2022-05-22 12:33:42 +0800

    ✨ enigma, initial commit

---
+++ 21Enigma/enigma.rb
@@ -0,0 +1,14 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+operation = gets.chomp
+pseudo_random_number = gets.to_i
+3.times do
+  rotor = gets.chomp
+end
+message = gets.chomp
+
+# Write an answer using puts
+# To debug: STDERR.puts "Debug messages..."
+
+puts "message"
