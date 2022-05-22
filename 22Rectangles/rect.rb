01c42dbdb1814821e70569bf5bea833959c242b0
Raskolnikov <jerry.tann@endress.com>
2022-05-22 13:47:12 +0800

    ✨ build points

--- 22Rectangles/rect.rb
+++ 22Rectangles/rect.rb
@@ -4,7 +4,7 @@
 w, h, count_x, count_y = gets.split(" ").collect { |x| x.to_i }
 inputs = gets.split(" ")
 xs = [0,w]
-ys = [0,y]
+ys = [0,h]
 for i in 0..(count_x-1)
   x = inputs[i].to_i
   xs << x
@@ -14,8 +14,14 @@ for i in 0..(count_y-1)
   y = inputs[i].to_i
   ys << y 
 end
-STDERR.puts "xs are #{xs}"
-STDERR.puts "ys are #{ys}"
+points = []
+xs.each do |i| 
+  ys.each do |j|
+    points << {:x => i, :y => j}
+  end
+end
+STDERR.puts "points are #{points}"
+STDERR.puts "12 is #{points.size}"
 
 # Write an answer using puts
 # To debug: STDERR.puts "Debug messages..."
