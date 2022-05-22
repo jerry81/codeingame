a1a1b5d359c2048365acb0eb227978703b8ef511
Raskolnikov <jerry.tann@endress.com>
2022-05-22 15:01:20 +0800

    ✨ game logic helper

--- 23RockPaperScissorsLizardSpock/rockpaper.rb
+++ 23RockPaperScissorsLizardSpock/rockpaper.rb
@@ -2,6 +2,70 @@
 # the standard input according to the problem statement.
 
 n = gets.to_i
+def play(i1,i2,s1,s2)
+  # returns the winner 
+  winner = i1 < i2 ? i1 : i2
+  case s1 
+  when 'R'
+    case s2 
+    when 'L'
+      winner = i1
+    when 'C'
+      winner = i1
+    when 'P'
+      winner = i2
+    when 'S'
+      winner = i2
+    end
+  when 'P'
+    case s2 
+    when 'S'
+      winner = i2
+    when 'C'
+      winner = i1
+    when 'R'
+      winner = i1
+    when 'L'
+      winner = i2
+    end
+  when 'C'
+    case s2 
+    when 'P'
+      winner = i1
+    when 'L'
+      winner = i1
+    when 'S'
+      winner = i2
+    when 'R'
+      winner = i2
+    end
+  when 'L'
+    case s2 
+    when 'S'
+      winner = i1
+    when 'P'
+      winner = i1
+    when 'R'
+      winner = i2
+    when 'S'
+      winner = i2
+    end
+  else # spock
+    case s2 
+    when 'C'
+      winner = i1
+    when 'R'
+      winner = i1
+    when 'L'
+      winner = i2
+    when 'P'
+      winner = i2
+    end
+  end
+  winner
+end
+
+STDERR.puts "expect 1 #{play(1,2,'R','C')}"
 n.times do
   numplayer, signplayer = gets.split(" ")
   numplayer = numplayer.to_i
