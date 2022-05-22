c39a59e3385806b52bad0294b2dbfb5922cc6350
Raskolnikov <jerry.tann@endress.com>
2022-05-22 12:26:36 +0800

    ✨ pass all tests

--- 20Logic/logic.rb
+++ 20Logic/logic.rb
@@ -23,16 +23,29 @@ end
 
 n = gets.to_i
 m = gets.to_i
+inputs = {}
 n.times do
   input_name, input_signal = gets.split(" ")
+  inputs[input_name] = input_signal
 end
+outputs = {}
 m.times do
   output_name, type, input_name_1, input_name_2 = gets.split(" ")
+  o = ""
+  i1 = inputs[input_name_1].split('')
+  i2 = inputs[input_name_2].split('')
+  i1.each_with_index do |v,i|
+    ch = v
+    ch2 = i2[i]
+    o << do_logic(type, ch, ch2)
+  end
+  outputs[output_name] = o
 end
-m.times do
+STDERR.puts "inputlookup is #{inputs}"
+outputs.each do |k,v|
 
   # Write an answer using puts
   # To debug: STDERR.puts "Debug messages..."
 
-  puts "output name and signal"
+  puts "#{k} #{v}"
 end
