0a57138c5313a50da762197aa3510dc537863bde
Raskolnikov <jerry.tann@endress.com>
2022-05-23 15:37:38 +0800

    ✨ start main loop

--- 24RevMinesweeper/rminesweeper.py
+++ 24RevMinesweeper/rminesweeper.py
@@ -11,8 +11,11 @@ for i in range(h):
     line = input()
     grid.append(line) # python has no list.push
 
+for y in grid:
+  for x in y:
+    print(f'i am iterating {x}', file=sys.stderr, flush=True)
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-print(f'Debug messages...{grid}', file=sys.stderr, flush=True)
+print(f'grid is {grid}', file=sys.stderr, flush=True)
 print("answer")
