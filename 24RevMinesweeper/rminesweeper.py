b1beeef946f0e1c7cf46e63f2af68010fe488a45
Raskolnikov <jerry.tann@endress.com>
2022-05-23 14:14:04 +0800

    ✨ minesweeper initial

---
+++ 24RevMinesweeper/rminesweeper.py
@@ -0,0 +1,18 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+w = int(input())
+h = int(input())
+grid = []
+for i in range(h):
+    line = input()
+    grid.append(line) # python has no list.push
+
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+print(f'Debug messages...{grid}', file=sys.stderr, flush=True)
+print("answer")
