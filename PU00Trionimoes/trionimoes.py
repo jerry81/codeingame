a537e17b44d6b1311f7c0bb82bb18cd04c3ec596
Raskolnikov <jerry.tann@endress.com>
2022-05-24 09:12:52 +0800

    ✨ finish planning strategy

--- PU00Trionimoes/trionimoes.py
+++ PU00Trionimoes/trionimoes.py
@@ -8,11 +8,17 @@ n = int(input())
 x, y = [int(i) for i in input().split()]
 
 # ideas
-# 1.  simplify?  represent shapes with 01, 11 where 0 is the hole
+# 1.  simplify?  represent shapes with 0,1,2,3 - UL, UR, DL, DR (location of hole)
 # 2.  helper method to detect a fillable hole?
 # 3.  "edge blocks" of 2x2 have limited choices
   # they always need to place the hole to the opposite direction of the corner it is in
   # unless they have the one hole specified in the input 
+# 4.  in the case of 8*8 
+  # the hole only appears in one place, so start with that 2*2 square 
+  # then it fills the 4*4 square because we handled that before
+  # then for the other 3 4*4 squares, they must leave a hole, so leave it in the corner away from the edge corner
+# 5.  helper to draw 
+
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
