d6211579f4fa2ddd612a785643337fb5710a16ca
Raskolnikov <jerry.tann@endress.com>
2022-05-25 10:06:16 +0800

    ✨ start main fn

--- PU00Trionimoes/trionimoes.py
+++ PU00Trionimoes/trionimoes.py
@@ -8,8 +8,8 @@ n = int(input())
 x, y = [int(i) for i in input().split()]
 
 # ideas
-# 1.  simplify?  represent shapes with 0,1,2,3 - UL, UR, DL, DR (location of hole)
-# 2.  helper method to detect a fillable hole?
+# 1.  simplify?  represent shapes with 0,1,2,3 - UL, UR, DL, DR (location of hole) - done 
+# 2.  helper method to detect a fillable hole? - todo 
 # 3.  "edge blocks" of 2x2 have limited choices
   # they always need to place the hole to the opposite direction of the corner it is in
   # unless they have the one hole specified in the input 
@@ -17,7 +17,11 @@ x, y = [int(i) for i in input().split()]
   # the hole only appears in one place, so start with that 2*2 square 
   # then it fills the 4*4 square because we handled that before
   # then for the other 3 4*4 squares, they must leave a hole, so leave it in the corner away from the edge corner
-# 5.  helper to draw 
+# 5.  helper to draw - done
+# 6.  improve helper to draw bigger squares 
+# 7.  solve the "abstract" problem where 2x2 blocks represented with 0-4
+
+
 
 def draw(type):
     l1 = '+--+--+'
@@ -87,7 +91,25 @@ if n == 1:
         if y == 1:
             t = 3
 
+def solve(order,x,y):
+    t = 0
+    if order == 1:
+        if x == 0:
+            if y == 0:
+                t = 0
+            if y == 1:
+                t = 2
+        if x == 1:
+            if y == 0:
+                t = 1
+            if y == 1:
+                t = 3
+        return [[t]]
+
+print(f"solve2 is {solve(1,1,1)}", file=sys.stderr, flush=True) 
+
 pretty_p(clean(draw(t)))
+
 
 
 # analysis
@@ -134,5 +156,31 @@ and
 
 3,2,3,2
 1,2(exception), 2(because hole needs to be "centralized"), 0
+3,1(centralize), 0(centralize), 2
+1,0,1,0
 
+3
+0 6
++--+--+--+--+--+--+--+--+
+|     |     |     |     |
++  +--+--+  +  +--+--+  +
+|  |     |  |  |     |  |
++--+  +--+--+--+--+  +--+
+|  |  |     |     |  |  |
++  +--+  +--+--+  +--+  +
+|     |  |     |  |     |
++--+--+--+--+  +--+--+--+
+|     |     |  |  |     |
++  +--+--+  +--+  +--+  +
+|  |     |  |     |  |  |
++--+--+  +--+--+--+  +--+
+|##|  |  |  |  |     |  |
++--+  +--+  +  +--+--+  +
+|     |     |     |     |
++--+--+--+--+--+--+--+--+
+
+3,2,3,2
+1,3,2,0
+3,2,0,2
+0*,0,1,0
 """
