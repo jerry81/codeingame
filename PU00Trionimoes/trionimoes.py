f40af5257ecc6b4db6a15eecc2d182ce77d264be
Raskolnikov <jerry.tann@endress.com>
2022-05-25 21:00:56 +0800

    🎨 clean up for submission

--- PU00Trionimoes/trionimoes.py
+++ PU00Trionimoes/trionimoes.py
@@ -1,9 +1,6 @@
 import sys
 import math
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-
 n = int(input())
 x, y = [int(i) for i in input().split()]
 
@@ -18,9 +15,6 @@ def make_grid(gn,gx,gy):
     return grid
 
 grid = make_grid(n,x,y)
-print(f"grid is {grid}", file=sys.stderr, flush=True) 
-top = y <= (2**n)//2 - 1 # hole is in top quadrant
-left = x <= (2**n)//2 - 1 # hole is in left quadrant 
 
 def find_hole(grid):
     l = len(grid)
@@ -31,8 +25,6 @@ def find_hole(grid):
     return False
 
 def divide_grid(grid, ox, oy, ox2, oy2):
-    # for recursion - need a stop condition 
-    # need to call myself 
     if ox2-ox <= 2: 
         return
     l = ox2-ox
@@ -42,7 +34,6 @@ def divide_grid(grid, ox, oy, ox2, oy2):
     ul_grid = list(map(lambda x: x[ox:ox+nl], u_grid))
     dl_grid = list(map(lambda x: x[ox:ox+nl], d_grid))
     ur_grid = list(map(lambda x: x[ox+nl:ox2], u_grid))
-    # dr_grid = list(map(lambda x: x[nl:oy2], d_grid))
     ulh = find_hole(ul_grid)
     urh = find_hole(ur_grid)
     dlh = find_hole(dl_grid)
@@ -70,26 +61,11 @@ def divide_grid(grid, ox, oy, ox2, oy2):
 dim = len(grid)
 divide_grid(grid,0,0,dim,dim)
 
-# prepare grid - separate original hole with the created holes 
-
-# make a new method to print the grid
 def pprint(grid,y,x):
-    # split into blocks of 2x2
     dim = len(grid)
     w = dim*3+1
     h = dim*2+1
-    # this is a trap! pgrid = [[' ']*w]*l 
     pgrid = [[' ' for _ in range(w)] for _ in range(h)]
-    # make first and last lines
-    # next two loops probably not necessary
-    # for idx in range(w//3):
-    #     pgrid[0][idx*3:(idx+1)*3] = ['+','-','-']
-    #     pgrid[0][w-1] = '+'
-    #     pgrid[h-1][idx*3:(idx+1)*3] = ['+','-','-']
-    #     pgrid[h-1][w-1] = '+'
-    # for idx in range(h):
-    #     pgrid[idx][0] = ['+', '|'][idx%2]
-    #     pgrid[idx][w-1] = ['+', '|'][idx%2]
     for idx_y in range(dim//2):
       for idx_x in range(dim//2):
           cy = idx_y*2
@@ -101,13 +77,11 @@ def pprint(grid,y,x):
           dl = grid[cy+1][cx]
           dr = grid[cy+1][cx+1]
           if ul == 'h':
-              # 4 lines 
               pgrid[py][px+3:px+7] =    list("+--+")
               pgrid[py+1][px+3:px+7] =  list("|  |")
               pgrid[py+2][px:px+7] = list("+--+  +")
               pgrid[py+3][px:px+7] = list("|     |")
               pgrid[py+4][px:px+7] = list("+--+--+")
-              # put a shape in the pgrid
           if ur == 'h':
               pgrid[py][px:px+4] =   list("+--+")
               pgrid[py+1][px:px+4] = list("|  |")
@@ -126,7 +100,6 @@ def pprint(grid,y,x):
               pgrid[py+2][px:px+7] = list("+  +--+")
               pgrid[py+3][px:px+4] = list("|  |")
               pgrid[py+4][px:px+4] = list("+--+")
-    # draw the hole 
     py = y*2
     px = x*3
     pgrid[py][px:px+4] =   "+--+"
@@ -136,59 +109,4 @@ def pprint(grid,y,x):
       joined="".join(i)
       print(joined) 
     
-"""
-given 
-divided is [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] 
-divided is [' ', 'h', 'h', ' ', ' ', 'h', 'h', ' '] 
-divided is [' ', 'h', ' ', ' ', ' ', ' ', 'h', ' '] 
-divided is [' ', ' ', ' ', 'h', 'h', ' ', ' ', ' '] 
-divided is [' ', ' ', ' ', ' ', 'h', ' ', ' ', ' '] 
-divided is [' ', 'h', 'h', ' ', ' ', ' ', 'h', ' '] 
-divided is ['o', ' ', 'h', ' ', ' ', 'h', 'h', ' '] 
-divided is [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] 
-
-+--+--+
-|     |
-+  +--+
-|  |
-+--+
-should print 
-+--+--+--+--+--+--+--+--+
-|     |     |     |     |
-+  +--+--+  +  +--+--+  +
-|  |     |  |  |     |  |
-+--+  +--+--+--+--+  +--+
-|  |  |     |     |  |  |
-+  +--+  +--+--+  +--+  +
-|     |  |     |  |     |
-+--+--+--+--+  +--+--+--+
-|     |     |  |  |     |
-+  +--+--+  +--+  +--+  +
-|  |     |  |     |  |  |
-+--+--+  +--+--+--+  +--+
-|##|  |  |  |  |     |  |
-+--+  +--+  +  +--+--+  +
-|     |     |     |     |
-+--+--+--+--+--+--+--+--+
-
-8x8 => 8*2 = 16 + 1 = 17 x 8*3 = 24 + 1 = 25
-
-divided is [' ', ' ', ' ', ' '] 
-divided is ['o', ' ', 'h', ' '] 
-divided is [' ', 'h', 'h', ' '] 
-divided is [' ', ' ', ' ', ' '] 
-
-should print 
-+--+--+--+--+
-|     |     |
-+--+  +--+  +
-|##|  |  |  |
-+--+--+  +--+
-|  |     |  |
-+  +--+--+  +
-|     |     |
-+--+--+--+--+
-
-4x4 -> 13x9
-"""
 pprint(grid,y,x)
