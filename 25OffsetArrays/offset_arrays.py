f323e3d72e48db147eba802ef561ffc9b0c28411
Raskolnikov <jerry.tann@endress.com>
2022-05-26 08:48:11 +0800

    ✅ pass a test

--- 25OffsetArrays/offset_arrays.py
+++ 25OffsetArrays/offset_arrays.py
@@ -16,12 +16,19 @@ for i in range(n):
     split_arr = rest.split('=')
     values = split_arr[1].strip().split(" ")
     start_idx = indicies.split('..')[0]
-    lookup[name] = { "s": start_idx, "v": values }
-    print(f"lookup is now  is {lookup}", file=sys.stderr, flush=True)
-
+    lookup[name] = { "s": int(start_idx), "v": values }
 x = input()
-
+split_input = x.split('[')
+name = split_input[0]
+rest = split_input[1]
+split_input = rest.split(']')
+idx = int(rest[0])
+print(f"name is {name} split input is now {split_input}", file=sys.stderr, flush=True)
+entry = lookup[name]
+arr = entry["v"]
+offset = entry["s"]
+idx = idx-offset
+print(arr[idx])
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-print("0")
