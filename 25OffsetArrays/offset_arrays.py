677866f4a32765c0ef97017b9cb0c3b7f4f05665
Raskolnikov <jerry.tann@endress.com>
2022-05-26 09:01:02 +0800

    ✅ pass test 2

--- 25OffsetArrays/offset_arrays.py
+++ 25OffsetArrays/offset_arrays.py
@@ -1,5 +1,5 @@
 import sys
-import math
+import re
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
@@ -22,12 +22,14 @@ split_input = x.split('[')
 name = split_input[0]
 rest = split_input[1]
 split_input = rest.split(']')
-idx = int(rest[0])
+print(f"name is {name} spl {split_input} and lookup is {lookup}", file=sys.stderr, flush=True)
+idx = int(split_input[0])
 print(f"name is {name} split input is now {split_input}", file=sys.stderr, flush=True)
 entry = lookup[name]
 arr = entry["v"]
 offset = entry["s"]
-idx = idx-offset
+print(f"idx is {idx} offset {offset}", file=sys.stderr, flush=True)
+idx = abs(offset - idx)
 print(arr[idx])
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
