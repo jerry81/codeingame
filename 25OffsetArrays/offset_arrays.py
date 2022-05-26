d317ad4cf2a4085896404b035f9b38b77b7c8ac0
Raskolnikov <jerry.tann@endress.com>
2022-05-26 09:30:02 +0800

    ✨ start handling nested case, still not there

--- 25OffsetArrays/offset_arrays.py
+++ 25OffsetArrays/offset_arrays.py
@@ -19,18 +19,24 @@ for i in range(n):
     lookup[name] = { "s": int(start_idx), "v": values }
 x = input()
 split_input = x.split('[')
-name = split_input[0]
-rest = split_input[1]
-split_input = rest.split(']')
-print(f"name is {name} spl {split_input} and lookup is {lookup}", file=sys.stderr, flush=True)
-idx = int(split_input[0])
-print(f"name is {name} split input is now {split_input}", file=sys.stderr, flush=True)
-entry = lookup[name]
-arr = entry["v"]
-offset = entry["s"]
-print(f"idx is {idx} offset {offset}", file=sys.stderr, flush=True)
-idx = abs(offset - idx)
-print(arr[idx])
+
+print(f"spl {split_input}", file=sys.stderr, flush=True)
+l = len(split_input)
+print(f"len is {len(split_input) }", file=sys.stderr, flush=True)
+# handle nesting 
+stack = split_input[:l-1]
+last = split_input[l-1]
+idx = int(last.split(']')[0])
+# until split_input only has 1 item left 
+cur_num = idx
+while len(stack) > 0:
+  name = stack.pop()
+  entry = lookup[name]
+  arr = entry["v"]
+  offset = entry["s"]
+  idx = abs(offset - idx)
+  cur_num = arr[idx]
+print(cur_num)
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
