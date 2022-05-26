b59d69ba4f57227acc98c38f426887c2769d4910
Raskolnikov <jerry.tann@endress.com>
2022-05-26 08:29:41 +0800

    ✨ parse

--- 25OffsetArrays/offset_arrays.py
+++ 25OffsetArrays/offset_arrays.py
@@ -3,11 +3,20 @@ import math
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
-
+lookup = {}
 n = int(input())
 for i in range(n):
     assignment = input()
-    print(f"should store {assignment}", file=sys.stderr, flush=True)
+    split_arr = assignment.split('[')
+    name = split_arr[0]
+    rest = split_arr[1]
+    split_arr = rest.split(']')
+    indicies = split_arr[0]
+    start_idx = indicies.split('..')[0]
+    print(f"rest is {rest}", file=sys.stderr, flush=True)
+    print(f"indicies are {indicies}", file=sys.stderr, flush=True)
+    lookup[name] = { "s": start_idx }
+    print(f"lookup is now  is {lookup}", file=sys.stderr, flush=True)
 x = input()
 
 # Write an answer using print
