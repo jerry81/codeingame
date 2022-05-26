ffe0feee25bd883aa61ec7ceed32d771d16fa6d4
Raskolnikov <jerry.tann@endress.com>
2022-05-26 08:35:28 +0800

    ✨ finish parsing

--- 25OffsetArrays/offset_arrays.py
+++ 25OffsetArrays/offset_arrays.py
@@ -12,11 +12,13 @@ for i in range(n):
     rest = split_arr[1]
     split_arr = rest.split(']')
     indicies = split_arr[0]
+    rest = split_arr[1]
+    split_arr = rest.split('=')
+    values = split_arr[1].strip().split(" ")
     start_idx = indicies.split('..')[0]
-    print(f"rest is {rest}", file=sys.stderr, flush=True)
-    print(f"indicies are {indicies}", file=sys.stderr, flush=True)
-    lookup[name] = { "s": start_idx }
+    lookup[name] = { "s": start_idx, "v": values }
     print(f"lookup is now  is {lookup}", file=sys.stderr, flush=True)
+
 x = input()
 
 # Write an answer using print
