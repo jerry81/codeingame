782ffecf0c5bf06ee38323ed7c3108c6ccf5ce20
Raskolnikov <jerry.tann@endress.com>
2022-05-26 08:21:18 +0800

    ✨ offset arrays initial

---
+++ 25OffsetArrays/offset_arrays.py
@@ -0,0 +1,16 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+for i in range(n):
+    assignment = input()
+    print(f"should store {assignment}", file=sys.stderr, flush=True)
+x = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("0")
