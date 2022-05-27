323a01b6ebe2e5b0e1fad93cd654d887be8ce274
Raskolnikov <jerry.tann@endress.com>
2022-05-27 13:58:55 +0800

    ✅ pass first test

--- 27PrefixCode/prefix_code.py
+++ 27PrefixCode/prefix_code.py
@@ -14,10 +14,21 @@ for i in range(n):
 s = input()
 
 sorted = {k: v for k,v in sorted(codes.items(), key=lambda item: len(item[0]))} 
-print(f"sorted is #{sorted}", file=sys.stderr, flush=True)
-
 # first step 
+result = []
+next_s = 0
+while next_s < len(s):
+  for k,v in sorted.items():
+      l = len(k)
+      sub = s[next_s:next_s+l]
+      if sub == k:
+          result.append(v)
+          next_s += l
+          print(f"match, sub is {sub} next_s is {next_s} result is {result}", file=sys.stderr, flush=True)
+          break
+
+print(f"result is {result}", file=sys.stderr, flush=True)
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-print("abracadabra")
+print(("").join(result))
