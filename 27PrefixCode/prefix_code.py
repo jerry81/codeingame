5bc1b71e4a908a7d5731215654b32bd2b3601fd1
Raskolnikov <jerry.tann@endress.com>
2022-05-27 12:46:49 +0800

    ✨ prefix code problem

---
+++ 27PrefixCode/prefix_code.py
@@ -0,0 +1,17 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+for i in range(n):
+    inputs = input().split()
+    b = inputs[0]
+    c = int(inputs[1])
+s = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("abracadabra")
