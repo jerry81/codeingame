81e7169e99173bc8c58695d577f4e2575ec2f1f8
Raskolnikov <jerry.tann@endress.com>
2022-05-27 14:06:23 +0800

    ✅ pass first 3 tests

--- 27PrefixCode/prefix_code.py
+++ 27PrefixCode/prefix_code.py
@@ -16,19 +16,27 @@ s = input()
 sorted = {k: v for k,v in sorted(codes.items(), key=lambda item: len(item[0]))} 
 # first step 
 result = []
+error = None
 next_s = 0
 while next_s < len(s):
+  found = False
   for k,v in sorted.items():
       l = len(k)
       sub = s[next_s:next_s+l]
       if sub == k:
           result.append(v)
           next_s += l
+          found = True
           print(f"match, sub is {sub} next_s is {next_s} result is {result}", file=sys.stderr, flush=True)
           break
+  if not found:
+    error = "DECODE FAIL AT INDEX " + str(next_s)
+    break
 
 print(f"result is {result}", file=sys.stderr, flush=True)
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
-print(("").join(result))
+if error:
+  print(error)
+else:
+  print(("").join(result))
