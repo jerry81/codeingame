b2a0382d6a0ab951b4dfea3f74805b113fcce677
Raskolnikov <jerry.tann@endress.com>
2022-05-27 11:23:25 +0800

    ✨ snail race - setup data structures

--- 26SnailRun/snail_run.py
+++ 26SnailRun/snail_run.py
@@ -5,14 +5,25 @@ import math
 # the standard input according to the problem statement.
 
 number_snails = int(input())
+snails = {}
 for i in range(number_snails):
     speed_snail = int(input())
+    snails[i+1] = {"s": speed_snail}
+goals = []
 map_height = int(input())
 map_width = int(input())
 for i in range(map_height):
     row = input()
+    for j in range(map_width):
+        c = row[j]
+        if c == '#':
+            goals.append({'x':j,'y':i})
+        elif c != '*':
+            snail = snails[int(c)]
+            snail['x'] = j
+            snail['y'] = i
 
-# Write an answer using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+print(f"snails are {snails}", file=sys.stderr, flush=True)
+print(f"goals are {goals}", file=sys.stderr, flush=True)
 
 print("winner is number")
