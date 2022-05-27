61e66c5a817081fc69ccafadb43fe9a0efe1edcd
Raskolnikov <jerry.tann@endress.com>
2022-05-27 13:04:29 +0800

    ✨ prefix - setup

--- 27PrefixCode/prefix_code.py
+++ 27PrefixCode/prefix_code.py
@@ -5,12 +5,18 @@ import math
 # the standard input according to the problem statement.
 
 n = int(input())
+codes = {}
 for i in range(n):
     inputs = input().split()
-    b = inputs[0]
-    c = int(inputs[1])
+    b = inputs[0] # key 
+    c = int(inputs[1]) # byte
+    codes[b] = chr(c)
 s = input()
 
+sorted = {k: v for k,v in sorted(codes.items(), key=lambda item: len(item[0]))} 
+print(f"sorted is #{sorted}", file=sys.stderr, flush=True)
+
+# first step 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
