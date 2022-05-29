863e20ef7ebbdfa16db3e4631dd4122265ba4c8d
Raskolnikov <jerry.tann@endress.com>
2022-05-29 18:05:17 +0800

    ✨ start bezier curve problem

---
+++ 28Bezier/bezier.py
@@ -0,0 +1,18 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+width, height = [int(i) for i in input().split()]
+steps = int(input())
+ax, ay = [int(i) for i in input().split()]
+bx, by = [int(i) for i in input().split()]
+cx, cy = [int(i) for i in input().split()]
+dx, dy = [int(i) for i in input().split()]
+for i in range(height):
+
+    # Write an answer using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+    print("curve")
