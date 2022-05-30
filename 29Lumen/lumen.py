dded6ef3f51736eb75be7128c589dcdd34e99b04
Raskolnikov <jerry.tann@endress.com>
2022-05-30 18:03:54 +0800

    ✨ setup candles map

--- 29Lumen/lumen.py
+++ 29Lumen/lumen.py
@@ -5,6 +5,7 @@ import math
 # the standard input according to the problem statement.
 
 grid = []
+candles = []
 n = int(input())
 l = int(input())
 for i in range(n):
@@ -12,10 +13,14 @@ for i in range(n):
     count = 0
     for cell in input().split():
         grid[i].append(cell)
+        if cell == 'C':
+            candles.append({"y":i, "x":count})
         count+=1
-        pass
+        
+
 
 print(f"grid is now {grid}", file=sys.stderr, flush=True)
+print(f"candles are {candles}", file=sys.stderr, flush=True)
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
