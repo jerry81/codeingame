eb70aa451611776896806cfd28e3c007a921ef5c
Raskolnikov <jerry.tann@endress.com>
2022-06-01 22:17:10 +0800

    ✅ pass all cases

--- 30ObjectInsertion/objectInsertion.py
+++ 30ObjectInsertion/objectInsertion.py
@@ -18,6 +18,7 @@ for i in range(c):
 
 # for each subblock, try to transpose the object onto it
 count = 0
+first_fit = {}
 for i in range(c-a+1):
     for j in range(d-b+1):
         fits = True
@@ -29,7 +30,20 @@ for i in range(c-a+1):
                 break
         if fits:
             count += 1
+            first_fit = {"x":j,"y":i}
         
 print(f"grid is {grid}", file=sys.stderr, flush=True)
 
 print(count)
+if count == 1:
+    print(f"firstfit is {first_fit}", file=sys.stderr, flush=True)
+    for k in mark_indexes:
+      dx = k["x"]
+      dy = k["y"]
+      i = first_fit["y"]
+      j = first_fit["x"]
+      line = list(grid[i+dy])
+      line[j+dx] = '*'
+      grid[i+dy] = "".join(line)
+    for l in grid:
+        print(l)
