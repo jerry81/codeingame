2bda99c5d596e1ea17bd4179be7a33d6b572ffbb
Raskolnikov <jerry.tann@endress.com>
2022-06-05 10:27:03 +0800

    ✨ initial commit

---
+++ 31SudokuValidator/sudokuvalidator.py
@@ -0,0 +1,16 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+grid = [[""]*9 for _ in range(9)]
+for i in range(9):
+    for j in input().split():
+        n = int(j)
+
+print(f"grid is {grid}", file=sys.stderr, flush=True)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("true or false")
