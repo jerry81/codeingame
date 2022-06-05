b6edcec7cc5d3ba5edb8f587bf1d69f73715db3c
Raskolnikov <jerry.tann@endress.com>
2022-06-05 10:41:13 +0800

    ✨ first attempt

--- 31SudokuValidator/sudokuvalidator.py
+++ 31SudokuValidator/sudokuvalidator.py
@@ -16,18 +16,41 @@ print(f"grid is {grid}", file=sys.stderr, flush=True)
 
 def validate(g):
     VALIDATOR = [1,2,3,4,5,6,7,8,9]
-    copy1 = VALIDATOR.copy()
     for i in range(9):
+        copy1 = VALIDATOR.copy()
         row = grid[i]
         for x in row:
             try:
                 copy1.remove(x)
             except:
-                print("false")
-                return 
-
+                return False
+        if len(copy1) != 0:
+            return False
+    for i in range(9):
+        copy2 = VALIDATOR.copy()
+        for j in grid:
+            try:
+                copy2.remove(j[i])
+            except:
+                return False 
+        if len(copy2) != 0:
+            return False 
+    for offsetx in range(0,9,3):
+        for offsety in range(0,9,3):
+          offsetx = 0
+          offsety = 0
+          copy3 = VALIDATOR.copy()
+          for i in range(3):
+              for j in range(3):
+                try:
+                    copy3.remove(grid[i+offsety][j+offsetx])
+                except: 
+                    return False
+          if len(copy3) != 0:
+              return False 
+    return True 
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
-validate(grid)
+res = "true" if validate(grid) else "false"
+print(res)
