fda99c392a5468a7e81fbf887a39747dc36c3f0d
Raskolnikov <jerry.tann@endress.com>
2022-06-07 14:13:12 +0800

    ✨ store grid

--- 32Labyrinth/labyrinth.py
+++ 32Labyrinth/labyrinth.py
@@ -11,12 +11,19 @@ r, c, a = [int(i) for i in input().split()]
 
 # game loop
 while True:
+    count = 0 
+    grid = []
     # kr: row where Rick is located.
     # kc: column where Rick is located.
     kr, kc = [int(i) for i in input().split()]
     for i in range(r):
         row = input()  # C of the characters in '#.TC?' (i.e. one line of the ASCII maze).
-
+        rl = list(row)
+        grid.append(rl)
+    count += 1
+    if count > 2:
+        break
+    print(f"grid is {grid}", file=sys.stderr, flush=True)
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
