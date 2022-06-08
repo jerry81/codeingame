07b0644025259cbcc0105eaf5974eaa4db4d4621
Raskolnikov <jerry.tann@endress.com>
2022-06-08 20:23:45 +0800

    ✨ try some exploration heuristics

--- 32Labyrinth/labyrinth.py
+++ 32Labyrinth/labyrinth.py
@@ -8,13 +8,21 @@ import math
 # c: number of columns.
 # a: number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
 r, c, a = [int(i) for i in input().split()]
+visited = {}
+breadcrumb = []
+def is_visited(key):
+  try:
+      _ = visited[key]
+      return True
+  except:
+      return False
 
 # game loop
 while True:
     count = 0 
     reached = False
     grid = []
-    visited = {}
+
     breadcrumb = []
     STATE = "EXPLORE"
     STATE = "FOUND"
@@ -23,10 +31,40 @@ while True:
     # kc: column where Rick is located.
     kr, kc = [int(i) for i in input().split()]
     visited[f"{kr},{kc}"] = True
+    
     for i in range(r):
         row = input()  # C of the characters in '#.TC?' (i.e. one line of the ASCII maze).
         rl = list(row)
         grid.append(rl)
+    ur = kr - 1 
+    dr = kr + 1 
+    lc = kc - 1 
+    rc = kc + 1 
+    if ur >= 0:
+      if grid[ur][kc] == '.' and not is_visited(f"{ur},{kc}"):
+          # move up
+          breadcrumb.append("DOWN")
+          print("UP")
+          continue
+    if dr < r:
+      if grid[dr][kc] == '.' and not is_visited(f"{dr},{kc}"):
+          # move down 
+          breadcrumb.append("UP")
+          print("DOWN")
+          continue
+    if lc >= 0:
+      if grid[kr][lc] == '.' and not is_visited(f"{kr},{lc}"):
+          # move left
+          breadcrumb.append("RIGHT")
+          print("LEFT")
+          continue
+    if rc < c:
+      if grid[kr][rc] == '.' and not is_visited(f"{kr},{rc}"):
+          # move right 
+          breadcrumb.append("LEFT")
+          print("RIGHT")
+          continue
+
     print(f"grid is {grid}", file=sys.stderr, flush=True)
     print(f"visited is {visited}")
     # Write an action using print
