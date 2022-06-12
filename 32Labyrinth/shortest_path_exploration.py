995f443bf7f9e512183572ed7d404179beddfe26
Raskolnikov <jerry.tann@endress.com>
2022-06-12 18:21:36 +0800

    🐛 fix shortest paths

--- 32Labyrinth/shortest_path_exploration.py
+++ 32Labyrinth/shortest_path_exploration.py
@@ -17,31 +17,54 @@ tg = [['.']*10 for _ in range(10)]
 for g in tg:
     print(g)
 
+for i in range(9):
+    tg[i][3] = 'x'
+
+for i in range(1,10):
+    tg[i][5] = 'x'
+
 def mark(grid,x,y,w,h):
     distances = defaultdict(lambda: 9999)
+    neighbors = [(y,x)]
     distances[y,x] = 0
-    neighbors = []
     visited = defaultdict(lambda: False)
-    visited[y,x] = True
-    d = y+1
-    u = y-1
-    r = x+1
-    l = x-1
-    if d < h:
-        if not visited[d,x]:
-          neighbors.append((d,x))
-    if u >= 0:
-        if not visited[u,x]:
-          neighbors.append((u,x))
-    if r < w:
-        if not visited[y,r]:
-          neighbors.append((y,r))
-    if l >= 0:
-        if not visited[y,l]:
-          neighbors.append((y,l))
-    return res
+    while len(neighbors) > 0:
+      ny,nx = neighbors.pop(0)
+      cur = distances[ny,nx]
+      visited[ny,nx] = True
+      d = ny+1
+      u = ny-1
+      r = nx+1
+      l = nx-1
+      if d < h:
+          grid_item = grid[d][nx]
+          if not visited[d,nx] and grid_item == '.':
+            neighbors.append((d,nx))
+      if u >= 0:
+          grid_item = grid[u][nx]
+          if not visited[u,nx] and grid_item == '.':
+            neighbors.append((u,nx))
+      if r < w:
+          grid_item = grid[ny][r]
+          if not visited[ny,r] and grid_item == '.':
+            neighbors.append((ny,r))
+      if l >= 0:
+          grid_item = grid[ny][l]
+          if not visited[ny,l] and grid_item == '.':
+            neighbors.append((ny,l))
+      for tup in neighbors:
+          distances[tup] = min(cur + 1, distances[tup])
+          visited[tup] = True 
+    return distances
 
-res = mark(tg,0,0)
+res = mark(tg,0,0,10,10)
+for item in res.items():
+    y,x = item[0]
+    tg[y][x] = item[1]
+
 print('r"')
-for r in res:
+for r in res.items():
     print(r)
+
+for t in tg:
+    print(t)
