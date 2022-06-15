88f35ce7974241ac682bd5057cb80a8dce8c9b0f
Raskolnikov <jerry.tann@endress.com>
2022-06-15 22:55:17 +0800

    ✅ pass more tests

--- 33VanEks/vaneks.py
+++ 33VanEks/vaneks.py
@@ -1,7 +1,5 @@
-from collections import defaultdict
-import sys
-import math
 
+import sys 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
@@ -10,12 +8,18 @@ n = int(input())
 
 memo = {}
 for i in range(n-1):
+  print(f"a1 is {a1}", file=sys.stderr, flush=True)
+  print(f"memo is {memo}", file=sys.stderr, flush=True)
   try: 
     lastIdx = memo[a1]
-    a1 = i - lastIdx
+    print(f"lastIdx of {a1} is {lastIdx}", file=sys.stderr, flush=True)
+    next = i - lastIdx
+    memo[a1] = i
+    a1 = next
   except:
     memo[a1] = i
-    a1 = 0
+    next = 0
+    a1 = next
   
 print(a1)
 # 0, 0, 1, 0, 2, 0, 2, 2, 1, 6,
@@ -23,3 +27,4 @@ print(a1)
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
+# 1, 0, 0, 1, 3, 0, 3, 2, 0, 3, 3, 0, 
