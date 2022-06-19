1878203493f4b8ad3bb8f46ba9f318458cab192b
Raskolnikov <jerry.tann@endress.com>
2022-06-19 18:43:13 +0800

    🎨 final touches, continous integratoin

--- AIGreenCircle/Bronze/sandbox.py
+++ AIGreenCircle/Bronze/sandbox.py
@@ -82,7 +82,7 @@ def get_best_possible_ci(pm,cimap):
           if v > bestv:
             bestv = v 
             best = int(i)
-    return str(best)
+    return f"CONTINUOUS_INTEGRATION {str(best)}"
 
 pm = {'TASK_PRIORITIZATION 8 0': True, 'TASK_PRIORITIZATION 8 1': True, 'TASK_PRIORITIZATION 8 2': True, 'TASK_PRIORITIZATION 8 3': True, 'TASK_PRIORITIZATION 8 4': True, 'TASK_PRIORITIZATION 8 5': True, 'TASK_PRIORITIZATION 8 6': True, 'TASK_PRIORITIZATION 8 7': True, 'TASK_PRIORITIZATION 5 0': True, 'TASK_PRIORITIZATION 5 1': True, 'TASK_PRIORITIZATION 5 2': True, 'TASK_PRIORITIZATION 5 3': True, 'TASK_PRIORITIZATION 5 4': True, 'TASK_PRIORITIZATION 5 5': True, 'TASK_PRIORITIZATION 5 6': True, 'TASK_PRIORITIZATION 5 7': True, 'CONTINUOUS_INTEGRATION 8': True, 'CONTINUOUS_INTEGRATION 3': True, 'RELEASE 8': True, 'RANDOM': True, 'WAIT': True}
 applications = {3: [4, 0, 0, 0, 4, 0, 0, 0], 7: [0, 4, 4, 0, 0, 0, 0, 0], 12: [0, 4, 0, 0, 0, 0, 0, 4], 18: [0, 0, 0, 4, 4, 0, 0, 0], 10: [0, 4, 0, 0, 0, 4, 0, 0], 17: [0, 0, 4, 0, 0, 0, 0, 4], 26: [0, 0, 0, 0, 0, 4, 0, 4], 6: [4, 0, 0, 0, 0, 0, 0, 4], 16: [0, 0, 4, 0, 0, 0, 4, 0], 8: [0, 4, 0, 4, 0, 0, 0, 0], 14: [0, 0, 4, 0, 4, 0, 0, 0]}
