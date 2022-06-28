e679df614686b770e8416e70ca0f3c2938dffbb1
Raskolnikov <jerry.tann@endress.com>
2022-06-28 20:33:30 +0800

    🐛  trying to cover the other cases

--- 34ReverseFizzBuzz/reverseFizzBuzz.py
+++ 34ReverseFizzBuzz/reverseFizzBuzz.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import sys
 import math
 
@@ -7,7 +8,7 @@ import math
 n = int(input())
 fizzIdxes = []
 buzzIdxes = []
-allIdxes = []
+allIdxes = defaultdict(lambda: -1)
 fizzBuzzI = []
 for i in range(n):
     line = input()
@@ -19,7 +20,7 @@ for i in range(n):
          buzzIdxes.append(i)
     if (line == 'FizzBuzz'):
         fizzBuzzI.append(i)
-    allIdxes.append({i:line})
+    allIdxes[i] = line
 print(f"fizzIdxes is {fizzIdxes}", file=sys.stderr, flush=True)
 print(f"buzzIdxes is {buzzIdxes}", file=sys.stderr, flush=True)
 print(f"all is {allIdxes}", file=sys.stderr, flush=True)
@@ -33,7 +34,22 @@ if len(buzzIdxes) == 0 and len(fizzBuzzI) >= 2:
     b = fizzBuzzI[1] - fizzBuzzI[0]
 if len(fizzIdxes) == 0 and len(fizzBuzzI) >= 2:
     f = fizzBuzzI[1] - fizzBuzzI[0]
-
+if len(fizzIdxes) == 1:
+    idx = fizzIdxes[0]
+    prevItem = allIdxes[idx-1]
+    if prevItem > -1 and prevItem != "Fizz" and prevItem != "Buzz" and prevItem != "FizzBuzz":
+        f = int(prevItem) + 1
+    nextItem = allIdxes[idx+1]
+    if nextItem > -1 and nextItem != "Fizz" and nextItem != "Buzz" and nextItem != "FizzBuzz":
+        f = int(nextItem) - 1
+if len(buzzIdxes) == 1:
+    idx = buzzIdxes[0]
+    prevItem = allIdxes[idx-1]
+    if prevItem > -1 and prevItem != "Fizz" and prevItem != "Buzz" and prevItem != "FizzBuzz":
+        b = int(prevItem) + 1
+    nextItem = allIdxes[idx+1]
+    if nextItem > -1 and nextItem != "Fizz" and nextItem != "Buzz" and nextItem != "FizzBuzz":
+        b = int(nextItem) - 1
     
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
