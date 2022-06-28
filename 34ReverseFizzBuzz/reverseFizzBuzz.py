c9abe7257c285606e1fba7ed1b898d0c9f1fc139
Raskolnikov <jerry.tann@endress.com>
2022-06-28 20:18:13 +0800

    ✅ pass 1 test

--- 34ReverseFizzBuzz/reverseFizzBuzz.py
+++ 34ReverseFizzBuzz/reverseFizzBuzz.py
@@ -10,15 +10,25 @@ buzzIdxes = []
 allIdxes = []
 for i in range(n):
     line = input()
-    if (line == 'fizz'):
+    if (line == 'Fizz'):
          print(f"fizz is {i}", file=sys.stderr, flush=True)
          fizzIdxes.append(i)
-         allIdxes.append({i:line})
-    print(f"fizz is {i}", file=sys.stderr, flush=True)
-        
+    if (line == 'Buzz'):
+         print(f"buzz is {i}", file=sys.stderr, flush=True)
+         buzzIdxes.append(i)
+    allIdxes.append({i:line})
+print(f"fizzIdxes is {fizzIdxes}", file=sys.stderr, flush=True)
+print(f"buzzIdxes is {buzzIdxes}", file=sys.stderr, flush=True)
+print(f"all is {allIdxes}", file=sys.stderr, flush=True)
+f = -1
+b = -1
+if len(fizzIdxes) >= 2:
+    f = fizzIdxes[1] - fizzIdxes[0]
+if len(buzzIdxes) >= 2:
+    b = buzzIdxes[1] - buzzIdxes[0]
 
-
+    
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-print("fizz buzz")
+print(f"{f} {b}")
