4aef829adef5d8b6bf9ff7142108a4a908739ab3
Raskolnikov <jerry.tann@endress.com>
2022-07-02 09:59:49 +0800

    ✨ WIP: split the string

--- 35LicensePlates/licensePlates.rs
+++ 35LicensePlates/licensePlates.rs
@@ -15,9 +15,21 @@ fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let n = parse_input!(input_line, i32);
-    eprintln!("error {:?}", x); // refresher on printing vars
+    eprintln!("input is {:?}", x); // refresher on printing vars
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
+    // experiments
+    let spl: Vec<&str>  = x.split("-").collect();
+    eprintln!("spl is {:?}", spl); 
+    let aChar = 'A' as u32;
+    let zChar = 'Z' as u32;
+    // split the input
+    // increment the number
+    // increment right chars
+    // increment left chars 
+    eprintln!("achar is {:?}", aChar); // refresher on printing vars
+
+    eprintln!("z is {:?}", zChar); // refresher on printing vars
 
     println!("AA-000-AA");
 }
