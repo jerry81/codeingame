5885cddd1b61196733150b1291c9ab12be4e3a82
Raskolnikov <jerry.tann@endress.com>
2022-07-10 18:08:32 +0800

    📝 int to str

--- 37TheRiver1/theriver.rs
+++ 37TheRiver1/theriver.rs
@@ -8,6 +8,12 @@ macro_rules! parse_input {
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+fn nextRiver(x:i64) -> i64 {
+    let as_str = x.to_string();
+    eprintln!("as_str {}", as_str);
+    return 55
+}
+
 fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
@@ -15,7 +21,8 @@ fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let r_2 = parse_input!(input_line, i64);
-
+    nextRiver(r_1);
+    nextRiver(r_2);
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
 
