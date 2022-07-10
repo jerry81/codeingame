7468a827b43604ec0cf98a22b6324b5b689050d5
Raskolnikov <jerry.tann@endress.com>
2022-07-10 18:48:29 +0800

    ✅ past first testcase

--- 37TheRiver1/theriver.rs
+++ 37TheRiver1/theriver.rs
@@ -22,23 +22,36 @@ fn next_river(x:i64) -> i64 {
 fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
-    let r_1 = parse_input!(input_line, i64);
+    let mut r_1 = parse_input!(input_line, i64);
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
-    let r_2 = parse_input!(input_line, i64);
+    let mut r_2 = parse_input!(input_line, i64);
     let mut river1 = HashSet::new();
     let mut river2 = HashSet::new();
-    let r1 = next_river(r_1);
-    let r2 = next_river(r_2);
-    if river1.contains(r2) {
-        eprintln!("river contains {}", r1);
+    river1.insert(r_1);
+    river2.insert(r_2);
+    let ans;
+    loop {
+      let r1 = next_river(r_1);
+      let r2 = next_river(r_2);
+      if river1.contains(&r2) {
+          eprintln!("hashmap is {:?}", river1);
+          eprintln!("river contains {}", r2);
+          ans = r2;
+          break
+      }
+      if river2.contains(&r1) {
+          eprintln!("hashmap is {:?}", river2);
+          ans = r1;
+          eprintln!("river contains {}", r1);
+          break
+      }
+      r_1 = r1;
+      r_2 = r2;
+      river1.insert(r1);
+      river2.insert(r2);
     }
-    if river2.contains(r1) {
-        eprintln!("river contains {}", r2);
-    }
-    river1.insert(r1);
-    river2.insert(r2);
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
-    println!("42");
+    println!("{}", ans);
 }
