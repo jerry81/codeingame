adedbcae98814ae35741c97cc213a04a0f3fa77b
Raskolnikov <jerry.tann@endress.com>
2022-07-10 18:14:07 +0800

    ✨ convert string to chars, then each char back to string and finally to int

--- 37TheRiver1/theriver.rs
+++ 37TheRiver1/theriver.rs
@@ -8,9 +8,14 @@ macro_rules! parse_input {
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-fn nextRiver(x:i64) -> i64 {
+fn next_river(x:i64) -> i64 {
     let as_str = x.to_string();
-    eprintln!("as_str {}", as_str);
+    let mut sum = 0;
+    for c in as_str.chars() {
+        sum+=(c.to_string()).parse::<i64>().unwrap();
+        
+    }
+    eprintln!("sum {}", sum);
     return 55
 }
 
@@ -21,8 +26,8 @@ fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let r_2 = parse_input!(input_line, i64);
-    nextRiver(r_1);
-    nextRiver(r_2);
+    next_river(r_1);
+    next_river(r_2);
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
 
