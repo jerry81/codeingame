512b881b05fa473174840fec3b648e1640150226
Raskolnikov <jerry.tann@endress.com>
2022-07-10 16:34:17 +0800

    ✅ pass 1 test

--- 36RobotShow/robotshow.rs
+++ 36RobotShow/robotshow.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp;
 
 macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
@@ -9,6 +10,7 @@ macro_rules! parse_input {
  * the standard input according to the problem statement.
  **/
 fn main() {
+    let mut ans = 0;
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let l = parse_input!(input_line, i32);
@@ -17,14 +19,19 @@ fn main() {
     let n = parse_input!(input_line, i32);
     let mut inputs = String::new();
     io::stdin().read_line(&mut inputs).unwrap();
+    let mut balls:Vec<i32> = Vec::new();
     for i in inputs.split_whitespace() {
         let b = parse_input!(i, i32);
+        balls.push(b);
+    if n == 1 {
+        ans = cmp::max(balls[0], l-balls[0]);
+    }
     }
 
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
 
-    println!("answer");
+    println!("{}",ans);
 }
 
 /* analysis
