943ddc5457bb32f3e7b831b44f120dbf4d301837
Raskolnikov <jerry.tann@endress.com>
2022-07-17 09:21:58 +0800

    ✨ til: rust function

--- 39CreditCardVerifierLuhns/ccverify.rs
+++ 39CreditCardVerifierLuhns/ccverify.rs
@@ -8,6 +8,11 @@ macro_rules! parse_input {
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+
+fn verify(card: String) -> bool {
+  return true;
+}
+
 fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
@@ -16,7 +21,8 @@ fn main() {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let card = input_line.trim_matches('\n').to_string();
-        
+        let res = verify(card);
+        eprintln!("res is {}", res);
     }
 
     // Write an answer using println!("message...");
