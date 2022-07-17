9a576f9601693d4536befabe2f3760e385ac2dc7
Raskolnikov <jerry.tann@endress.com>
2022-07-17 16:09:11 +0800

    ✨ til rust struct

--- 41Hyperduals/hyperduals.rs
+++ 41Hyperduals/hyperduals.rs
@@ -8,17 +8,31 @@ macro_rules! parse_input {
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+
+struct Horse {
+    v: i32,
+    e: i32,
+}
 fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let n = parse_input!(input_line, i32);
+    let mut horses = Vec::new();
     for i in 0..n as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let inputs = input_line.split(" ").collect::<Vec<_>>();
         let v = parse_input!(inputs[0], i32);
         let e = parse_input!(inputs[1], i32);
+        let s = v+e;
+        let mut h = Horse {
+          v: v,
+          e: e,
+          s: s
+        };
+        horses.push(h);
     }
+    eprintln!("horses len is {}", horses.len());
 
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
