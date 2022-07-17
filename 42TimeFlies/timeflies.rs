bc5b2bced1d45f82659b144b91b4d14b5d093c4d
Raskolnikov <jerry.tann@endress.com>
2022-07-17 17:01:18 +0800

    ✨ timeflies, initial

---
+++ 42TimeFlies/timeflies.rs
@@ -0,0 +1,25 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let begin = input_line.trim().to_string();
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let end = input_line.trim().to_string();
+    let bs = begin.split('.');
+    let es = end.split('.');
+    
+    // Write an answer using println!("message...");
+    // To debug: eprintln!("Debug message...");
+
+    println!("YY year[s], MM month[s], total NN days");
+}
