59ee19a9b8ac570f64a84c9783761be68cd17cbf
Raskolnikov <jerry.tann@endress.com>
2022-07-17 11:40:12 +0800

    ✨ setup hashes

--- 40BracketsExtreme/brackets_extreme.rs
+++ 40BracketsExtreme/brackets_extreme.rs
@@ -12,7 +12,19 @@ fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let expression = input_line.trim().to_string();
-
+    
+    let lookup = HashMap::new();
+    let openers = HashSet::new();
+    let closers = HashSet::new();
+    openers.insert('[');
+    openers.insert('(');
+    openers.insert('{');
+    closers.insert(']');
+    closers.insert(')');
+    closers.insert('}');
+    lookup.insert(']', '[');
+    lookup.insert(')', '(');
+    lookup.insert('}', '{');
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
 
