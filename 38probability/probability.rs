48baf96d9665e954998c9fbcae59d44e64036227
Raskolnikov <jerry.tann@endress.com>
2022-07-17 15:59:51 +0800

    ✨ hyperduals, initial

--- 38probability/probability.rs
+++ 38probability/probability.rs
@@ -26,5 +26,26 @@ fn main() {
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
 
+    // n choose k aka binomial coefficient 
+    // \binom {n}{k}=\frac {n!}{k!(n-k)!}
+    // n is 38, k is 7 
+    // 6 or 7 distinct out of 7 tries 
+    // 1 * 37/38 * 36/38 * 35/38 * 34/38 * 33/38 * 32/38 ( all distinct)
+    // 6 out of 7 distinct 
+    // 1 * 37/38 * 36/38 * 35/38 * 34/38 * 33/38 * 6/38 = 
+
+    // different approach
+
+    // total combinations 
+    // 38 ^ 7 
+    // total acceptable combinations 
+    // 7 distinct 
+    // 38*37*36*35*34*32*31
+    // 6 distinct 
+    // 7 choose 1 to repeat 
+
+​
+ 
+
     println!("probability");
 }
---
+++ 41Hyperduals/hyperduals.rs
@@ -0,0 +1,27 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let n = parse_input!(input_line, i32);
+    for i in 0..n as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let v = parse_input!(inputs[0], i32);
+        let e = parse_input!(inputs[1], i32);
+    }
+
+    // Write an answer using println!("message...");
+    // To debug: eprintln!("Debug message...");
+
+    println!("42");
+}
