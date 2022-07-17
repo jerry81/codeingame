79b4bb55a7252b847fdefc6109073b9c2039ed71
Raskolnikov <jerry.tann@endress.com>
2022-07-17 16:18:07 +0800

    ✨ pass most tests

--- 41Hyperduals/hyperduals.rs
+++ 41Hyperduals/hyperduals.rs
@@ -12,6 +12,7 @@ macro_rules! parse_input {
 struct Horse {
     v: i32,
     e: i32,
+    s: i32
 }
 fn main() {
     let mut input_line = String::new();
@@ -25,17 +26,26 @@ fn main() {
         let v = parse_input!(inputs[0], i32);
         let e = parse_input!(inputs[1], i32);
         let s = v+e;
-        let mut h = Horse {
+        let h = Horse {
           v: v,
           e: e,
           s: s
         };
         horses.push(h);
     }
+    horses.sort_by(|a, b| b.s.cmp(&a.s));
     eprintln!("horses len is {}", horses.len());
-
+    let mut min = std::i32::MAX;
+    for i in 0..horses.len()-1 {
+      let horse1 = &horses[i];
+      let horse2 = &horses[i+1];
+      let diff = (horse1.v - horse2.v).abs() + (horse1.e - horse2.e).abs();
+      if diff < min {
+          min = diff;
+      }
+    }
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
 
-    println!("42");
+    println!("{}", min);
 }
