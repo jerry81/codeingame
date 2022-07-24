4dcae679751383a848cf472624043d7d0a2ea72d
Raskolnikov <jerry.tann@endress.com>
2022-07-24 22:34:45 +0800

    ✨ is the king in check, initial

---
+++ 43KingInCheck/kingincheck.rs
@@ -0,0 +1,22 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+    for i in 0..8 as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let chess_row = input_line.trim_matches('\n').to_string();
+    }
+
+    // Write an answer using println!("message...");
+    // To debug: eprintln!("Debug message...");
+
+    println!("Check/No Check");
+}
