f68ed7919e79987505514c8cb6834ca613433371
Raskolnikov <jerry.tann@endress.com>
2022-07-25 11:17:03 +0800

    🐛 oops, there were spaces between characters

--- 43KingInCheck/kingincheck.rs
+++ 43KingInCheck/kingincheck.rs
@@ -9,10 +9,15 @@ fn main() {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let chess_row = input_line.trim_matches('\n').to_string();
-        let chess_chars: Vec<char> = chess_row.chars().collect();
+        let chess_chars: Vec<&str> = chess_row.split(" ").collect();
         for j in 0..8 as usize {
           let c = chess_chars[j];
-          eprintln!("c is {}", c);
+          if c == "K" {
+            eprintln!("king found {} {}", i, j);
+          }
+          else if c != "_" {
+            eprintln!("enemy found {} {}", i, j);
+          }
         }
     }
 
