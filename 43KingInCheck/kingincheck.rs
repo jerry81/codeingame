213c70a6791c9f5073e004dfc42fb10c6f131afa
Raskolnikov <jerry.tann@endress.com>
2022-07-25 12:21:58 +0800

    ✨ handle case, rook

--- 43KingInCheck/kingincheck.rs
+++ 43KingInCheck/kingincheck.rs
@@ -5,32 +5,33 @@ use std::io;
  * the standard input according to the problem statement.
  **/
 fn main() {
+  struct Enemy {
+    x: usize,
+    y: usize,
+    piece: String
+  }
+
+  struct King {
+    x: usize,
+    y: usize
+  }
+
+  let mut king = King {
+    x: 0,
+    y: 0
+  };
+
+  let mut enemy = Enemy {
+    x: 0,
+    y: 0,
+    piece: "".to_string()
+  };
     for i in 0..8 as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let chess_row = input_line.trim_matches('\n').to_string();
         let chess_chars: Vec<&str> = chess_row.split(" ").collect();
-        struct Enemy {
-          x: usize,
-          y: usize,
-          piece: String
-        }
 
-        struct King {
-          x: usize,
-          y: usize
-        }
-
-        let mut king = King {
-          x: 0,
-          y: 0
-        };
-
-        let mut enemy = Enemy {
-          x: 0,
-          y: 0,
-          piece: "".to_string()
-        };
 
         for j in 0..8 as usize {
           let c = chess_chars[j];
@@ -45,23 +46,25 @@ fn main() {
           }
         }
 
-        let p = enemy.piece;
-        eprintln!("p as str {:?}", p.as_str());
+
+    }
+    let p = enemy.piece;
+        let mut returned = "Check";
         // one of B, N, R, Q
         match p.as_str() {
           "N"=>eprintln!("knight"),
           "Q"=>eprintln!("queen"),
           "R"=>{
-            eprintln!("rook!");
-            break
+            if enemy.x != king.x && enemy.y != king.x {
+              returned = "No Check";
+            }
           },
           "B"=>eprintln!("bishop"),
           _=> unreachable!()
         }
-    }
 
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
 
-    println!("Check/No Check");
+    println!("{}", returned);
 }
