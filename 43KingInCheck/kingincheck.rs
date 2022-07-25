697749a5264bf6925efe85f56c420aa12e19b198
Raskolnikov <jerry.tann@endress.com>
2022-07-25 11:09:10 +0800

    ✨ convert string into Vec<char>

--- 43KingInCheck/kingincheck.rs
+++ 43KingInCheck/kingincheck.rs
@@ -1,9 +1,5 @@
 use std::io;
 
-macro_rules! parse_input {
-    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
-}
-
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -13,6 +9,11 @@ fn main() {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let chess_row = input_line.trim_matches('\n').to_string();
+        let chess_chars: Vec<char> = chess_row.chars().collect();
+        for j in 0..8 as usize {
+          let c = chess_chars[j];
+          eprintln!("c is {}", c);
+        }
     }
 
     // Write an answer using println!("message...");
