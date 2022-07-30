edb2b80a5b79c456c81b7b716e119c317ab7bb73
Raskolnikov <jerry.tann@endress.com>
2022-07-30 13:29:27 +0800

    ✨  get unique values of list

--- 38probability/probability.rs
+++ 38probability/probability.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::collections::HashSet;
 
 macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
@@ -9,7 +10,14 @@ macro_rules! parse_input {
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+fn filter_uniq(vec: Vec<u8>) -> Vec<u8> {
+    vec.into_iter()
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect()
+}
 fn main() {
+    eprintln!("test filter uniq {:?}", filter_uniq(vec![1,2,3,3]));
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let m = parse_input!(input_line, i32);
@@ -23,5 +31,7 @@ fn main() {
         let random = rng.gen_range(0..38);
         v.push(random);
     }
+    let u = filter_uniq(v);
+    eprintln!("u is {:?}", u);
     println!("probability");
 }
