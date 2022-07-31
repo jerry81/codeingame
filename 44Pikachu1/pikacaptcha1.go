fdf9d140c2c19b4ea6560abb03910775ff79def9
Raskolnikov <jerry.tann@endress.com>
2022-07-31 16:52:04 +0800

    ✨ pikaptcha initial

---
+++ 44Pikachu1/pikacaptcha1.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+func main() {
+    var width, height int
+    fmt.Scan(&width, &height)
+
+    for i := 0; i < height; i++ {
+        var line string
+        fmt.Scan(&line)
+    }
+    for i := 0; i < height; i++ {
+
+        // fmt.Fprintln(os.Stderr, "Debug messages...")
+        fmt.Println("#####")// Write action to stdout
+    }
+}
