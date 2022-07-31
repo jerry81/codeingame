9dcef06c40d4ed0c50b4c3d71e1680b17d52baa5
Raskolnikov <jerry.tann@endress.com>
2022-07-31 17:27:33 +0800

    ✨ work on main logic

--- 44Pikachu1/pikacaptcha1.go
+++ 44Pikachu1/pikacaptcha1.go
@@ -22,8 +22,41 @@ func main() {
 				chars := []rune(line) // :- is declaration plus assignment
         lines = append(lines, chars)
     }
+		newarr := lines
     for i := 0; i < height; i++ {
-        fmt.Fprintln(os.Stderr, "i is ", i, "line[i] is ", lines[i])
+        for j := 0; j < width; j++ {
+					up := i - 1
+          down := i + 1
+					left := j - 1
+					right := j + 1
+					cnt := 0
+					ch := string(lines[i][j])
+					if up >= 0 {
+						if string(lines[up][j]) == '0' {
+							cnt += 1
+						}
+					}
+					if down < height {
+						if string(lines[down][j]) == '0' {
+							cnt += 1
+						}
+					}
+					if left >= 0 {
+						if string(lines[i][left]) == '0' {
+						  cnt += 1
+						}
+					}
+					if right < width {
+						if string(lines[i][right]) == '0' {
+						  cnt += 1
+						}
+					}
+					if string(lines[i][j]) == '#' {
+					  newarr[i][j] = '#'
+					} else {
+						newarr[i][j] = string(cnt)
+				  }
+				}
         fmt.Println("#####")// Write action to stdout
     }
 }
