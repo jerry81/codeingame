f7d0b1ebca2c61ca292830aef8ef42c10732252a
Raskolnikov <jerry.tann@endress.com>
2022-07-31 18:26:24 +0800

    ✨ classic deep copy issue

--- 44Pikachu1/pikacaptcha1.go
+++ 44Pikachu1/pikacaptcha1.go
@@ -23,43 +23,50 @@ func main() {
 		    chars := []rune(line) // :- is declaration plus assignment
         lines = append(lines, chars)
     }
-		newarr := lines
+		var newarr = make([][]rune,height)
+		for i := range newarr {
+			newarr[i] = make([]rune, width)
+	  }
     for i := 0; i < height; i++ {
         for j := 0; j < width; j++ {
 					up := i - 1
-                    down := i + 1
+          down := i + 1
 					left := j - 1
 					right := j + 1
 					cnt := 0
-					ch := string(lines[i][j])
-					fmt.Fprintln(os.Stderr, "ch is  ", ch)
+					// ch := string(lines[i][j])
 					if up >= 0 {
 						if string(lines[up][j]) == "0" {
+							fmt.Fprintln(os.Stderr, "up detected")
 							cnt += 1
 						}
 					}
 					if down < height {
 						if string(lines[down][j]) == "0" {
+							fmt.Fprintln(os.Stderr, "down detected")
 							cnt += 1
 						}
 					}
 					if left >= 0 {
 						if string(lines[i][left]) == "0" {
+							fmt.Fprintln(os.Stderr, "left detected")
 						  cnt += 1
 						}
 					}
 					if right < width {
 						if string(lines[i][right]) == "0" {
+							fmt.Fprintln(os.Stderr, "right detected")
 						  cnt += 1
 						}
 					}
+                    fmt.Fprintln(os.Stderr, "cnt is ", cnt)
 					if string(lines[i][j]) == "#" {
 					  newarr[i][j] = rune('#')
-            fmt.Fprintln(os.Stderr, "newarr[i][j] is now  ", newarr[i][j])
+                      fmt.Fprintln(os.Stderr, "i is ", i, " j is ", j, " newarr[i][j] is now  ", rune('#'))
 					} else {
                         asStr := strconv.Itoa(cnt)
-                    fmt.Fprintln(os.Stderr, "string is ",)
-					  newarr[i][j] = []rune(asStr)[0]
+                        fmt.Fprintln(os.Stderr, "i is ", i, " j is ", j, " newarr[i][j] is now  ", []rune(asStr)[0])
+					    newarr[i][j] = []rune(asStr)[0]
 				  }
 				}
         for i := 0; i < height; i++ {
