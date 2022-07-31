09c284a88506885ff5e59310001d45bfbfa855a4
Raskolnikov <jerry.tann@endress.com>
2022-07-31 18:43:58 +0800

    ✅ pass more tests

--- 44Pikachu1/pikacaptcha1.go
+++ 44Pikachu1/pikacaptcha1.go
@@ -28,12 +28,6 @@ func main() {
 			newarr[i] = make([]rune, width)
 	  }
 
-		newarr[1][0] = rune('a')
-		newarr[1][1] = rune('b')
-		newarr[1][2] = rune('c')
-        newarr[1][3] = rune('d')
-        newarr[1][4] = rune('e')
-		fmt.Fprintln(os.Stderr, "newarr ", string(newarr[1]))
 
     for i := 0; i < height; i++ {
         for j := 0; j < width; j++ {
@@ -45,32 +39,28 @@ func main() {
 					// ch := string(lines[i][j])
 					if up >= 0 {
 						if string(lines[up][j]) == "0" {
-							fmt.Fprintln(os.Stderr, "up detected")
+							// fmt.Fprintln(os.Stderr, "up detected")
 							cnt += 1
 						}
 					}
 					if down < height {
 						if string(lines[down][j]) == "0" {
-							fmt.Fprintln(os.Stderr, "down detected")
 							cnt += 1
 						}
 					}
 					if left >= 0 {
 						if string(lines[i][left]) == "0" {
-							fmt.Fprintln(os.Stderr, "left detected")
 						  cnt += 1
 						}
 					}
 					if right < width {
 						if string(lines[i][right]) == "0" {
-							fmt.Fprintln(os.Stderr, "right detected")
 						  cnt += 1
 						}
 					}
                     fmt.Fprintln(os.Stderr, "cnt is ", cnt)
 					if string(lines[i][j]) == "#" {
 					  newarr[i][j] = rune('#')
-                      fmt.Fprintln(os.Stderr, "newarr is now ", newarr)
 					} else {
                         asStr := strconv.Itoa(cnt)
 
@@ -80,10 +70,7 @@ func main() {
 				}
 
     }
-		fmt.Fprintln(os.Stderr, "final newarr is ", newarr)
                 for i := 0; i < height; i++ {
-                    fmt.Fprintln(os.Stderr, "final print i is now ", i)
-                    fmt.Fprintln(os.Stderr, "should print  ", string(newarr[i]))
 					fmt.Println(string(newarr[i]))
 				}  // Write action to stdout
 }
