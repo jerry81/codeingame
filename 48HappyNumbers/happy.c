1e594cc60ea2b0858891c3c72a8563f9d80feb59
Raskolnikov <jerry.tann@endress.com>
2022-08-07 16:54:08 +0800

    ✨ TIL: strings not assignable, use strcpy

--- 48HappyNumbers/happy.c
+++ 48HappyNumbers/happy.c
@@ -1,28 +1,3 @@
-// Goal
-// A happy number is defined by the following process:
-// Starting with any positive integer, replace the number by the sum of the squares of its digits in base-ten, and repeat the process until the number either equals 1 (where it will stay), or it loops endlessly in a cycle that does not include 1. Those numbers for which this process ends in 1 are happy numbers, while those that do not end in 1 are unhappy numbers.
-
-// Given a list of numbers, classify each of them as happy or unhappy.
-// Input
-// Line 1: An integer N for the number of numbers to test.
-// Following N Lines: Each line has a positive integer you should test whether it is happy or not.
-
-// Be aware that some input numbers are really BIG, much bigger than your Integer type can handle. Find a way to overcome it.
-// Output
-// Output N lines.
-// Following the same order as inputs, each line starts with a given number from the list, a space, and then an ascii art :) or :( to indicate this number is happy or unhappy.
-// Constraints
-// 1 ≤ N ≤ 100
-// 0 < each number ≤ 10^26
-// Example
-// Input
-// 2
-// 23
-// 24
-// Output
-// 23 :)
-// 24 :(
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -37,9 +12,14 @@ int main()
 {
     int N;
     scanf("%d", &N); fgetc(stdin);
+    char xs[N][129];
     for (int i = 0; i < N; i++) {
         char x[129];
         scanf("%[^\n]", x); fgetc(stdin);
+        strcpy(xs[i],x);
+    }
+    for (int i = 0; i < N; i++) {
+        fprintf(stderr, "xs[i] is %s\n", xs[i]);
     }
 
     // Write an answer using printf(). DON'T FORGET THE TRAILING \n
