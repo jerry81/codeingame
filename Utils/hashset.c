b0fc8cf6f8d92dcd2cc8e2c55106ed6ac35514ed
Raskolnikov <jerry.tann@endress.com>
2022-08-08 12:34:36 +0800

    📝 figure out how to use example hash

--- Utils/hashset.c
+++ Utils/hashset.c
@@ -12,12 +12,12 @@ struct nlist { /* table entry: */
 static struct nlist *hashtab[HASHSIZE]; /* pointer table */
 
 /* hash: form hash value for string s */
-unsigned hash(char *s)
+unsigned hash(char *s) // goal: convert string into an index in the hash table
 {
     unsigned hashval;
     for (hashval = 0; *s != '\0'; s++)
       hashval = *s + 31 * hashval;
-    return hashval % HASHSIZE;
+    return hashval % HASHSIZE; // hash function always ends with this line
 }
 
 /* lookup: look for s in hashtab */
@@ -27,6 +27,7 @@ struct nlist *lookup(char *s)
     for (np = hashtab[hash(s)]; np != NULL; np = np->next)
         if (strcmp(s, np->name) == 0)
           return np; /* found */
+    // printf("not found!");
     return NULL; /* not found */
 }
 
@@ -64,7 +65,21 @@ int main() {
   install("key1", "value1");
   install("key2", "value2");
   entry = *lookup("key1");
-  printf("entry is %s", entry.defn);
+  if (entry.defn == NULL) {
+    printf("not found!");
+  } else {
+    printf("entry is %s", entry.defn);
+  }
+
+  if (lookup("key3") == NULL) {
+    printf("key was not found!\n");
+  }
+  // printf("lookup done ");
+  // if (entry2.defn == NULL) {
+  //   printf("not found!");
+  // } else {
+  //   printf("entry2 is %s", entry2.defn);
+  // }
   return 0;
 }
 
