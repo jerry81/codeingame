90e18c1bcf78e40d3dc3559482d30aab1005c28a
Raskolnikov <jerry.tann@endress.com>
2022-08-14 20:47:13 +0800

    🚧 inching closer

--- 49CheckDigit/check_digit.c
+++ 49CheckDigit/check_digit.c
@@ -58,82 +58,100 @@ Output
  * the standard input according to the problem statement.
  **/
 
-char is_valid(char isbn[21]) {
+char is_valid(char isbn[21])
+{
   int i = 0;
-  while (isbn[i] != '\0') {
+  while (isbn[i] != '\0')
+  {
     ++i;
   }
-  fprintf(stderr, "len is %i\n", i);
-  if (i != 10 && i != 13) {
+
+  if (i != 10 && i != 13)
+  {
     return 0;
   }
 
-  if (i == 10) {
+  if (i == 10)
+  {
     // convert each char to digit and get checksum
     int sum = 0;
-    for (int j = 0; j < 9; j+=1) {
+    for (int j = 0; j < 9; j += 1)
+    {
       int asI = isbn[j] - '0';
-      if (asI >= 10) {
-          return 0;
+      if (asI >= 10)
+      {
+        return 0;
       }
       int multiplier = 10 - j;
       sum += multiplier * asI;
     }
-    int expect = sum%11;
+    int expect = 11 - (sum % 11);
     char last = isbn[9];
-    if (expect == 10) {
+    if (expect == 10)
+    {
       return last == 'X';
-    } else {
+    }
+    else
+    {
       char asC = expect + '0';
+      fprintf(stderr, "asC %c, last %c\n", asC, last);
       return asC == last;
     }
-  } else if (i == 13) {
+  }
+  else if (i == 13)
+  {
     int sum = 0;
-    for (int j = 0; j < 12; j+=1) {
+    for (int j = 0; j < 12; j += 1)
+    {
       int asI = isbn[j] - '0'; // TODO: DRY
-      if (asI >= 10) {
-          return 0;
+      if (asI >= 10)
+      {
+        return 0;
       }
-      if (j%2 == 0) {
+
+      if (j % 2 == 0)
+      {
         sum += asI;
-      } else {
-        sum += asI*3;
       }
-      int expect = sum % 10;
+      else
+      {
+        sum += (asI * 3);
+      }
+      int expect = 10 - (sum % 10);
       char last = isbn[12];
       char asC = expect + '0';
       return last == asC;
-    // convert each char to digit and get checksum
+      // convert each char to digit and get checksum
+    }
   }
 }
 
 int main()
 {
-    int N;
-    scanf("%d", &N); fgetc(stdin); // fget c: gets character from file pointer - must be reading the new line
-    char ISBNs[N][21];
-    int count = 0;
-    for (int i = 0; i < N; i++) {
-        char ISBN[21];
-        scanf("%[^\n]", ISBN); fgetc(stdin);
-        strcpy(ISBNs[i],ISBN);
-        fprintf(stderr, "Isbns is %s\n", ISBNs[i]);
-        if (!is_valid(ISBN)) {
-          count+=1;
-        }
+  int N;
+  scanf("%d", &N);
+  fgetc(stdin); // fget c: gets character from file pointer - must be reading the new line
+  char ISBNs[N][21];
+  int count = 0;
+  for (int i = 0; i < N; i++)
+  {
+    char ISBN[21];
+    scanf("%[^\n]", ISBN);
+    fgetc(stdin);
+    strcpy(ISBNs[i], ISBN);
+    if (!is_valid(ISBN))
+    {
+      count += 1;
     }
-    printf("%i invalid:\n", count);
-    for (int j = 0; j < count; ++j) {
+  }
+  printf("%i invalid:\n", count);
+  for (int j = 0; j < count; ++j)
+  {
+    printf("%s\n", ISBNs[j]);
+  }
 
-    }
+  // Write an answer using printf(). DON'T FORGET THE TRAILING \n
+  // To debug: fprintf(stderr, "Debug messages...\n");
 
-
-
-
-    // Write an answer using printf(). DON'T FORGET THE TRAILING \n
-    // To debug: fprintf(stderr, "Debug messages...\n");
-
-    printf("answer\n");
-
-    return 0;
+  return 0;
 }
