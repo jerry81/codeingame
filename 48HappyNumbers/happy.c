71d5741faa8948ada6e33950340dd29ac7d60eaa
Raskolnikov <jerry.tann@endress.com>
2022-08-14 16:39:19 +0800

    ✨ TIL: in c, order of declaration matters, no function hoisting

--- 48HappyNumbers/happy.c
+++ 48HappyNumbers/happy.c
@@ -3,6 +3,57 @@
 #include <string.h>
 #include <stdbool.h>
 
+
+struct nlist2 { /* table entry: */
+    struct nlist2 *next; /* next entry in chain */
+    int name; /* defined name */
+    int defn; /* replacement text */
+};
+
+#define HASHSIZE 101
+struct nlist2 *hashtab2[HASHSIZE];
+struct nlist2 *orig[HASHSIZE];
+
+unsigned hash2(int i) {
+  return i%HASHSIZE;
+}
+
+void reset_ht() {
+  memcpy( hashtab2, orig, sizeof hashtab2 );
+}
+
+/* lookup: look for s in hashtab */
+struct nlist2 *lookup2(int i)
+{
+    struct nlist2 *np;
+    for (np = hashtab2[hash2(i)]; np != NULL; np = np->next) // NULL in C is null pointer
+        if (i == np->name)
+          return np; /* found */
+    // printf("not found!");
+    return NULL; /* not found */
+}
+
+struct nlist2 *install2(int name, int defn)
+{
+    struct nlist2 *np;
+    unsigned hashval;
+    if ((np = lookup2(name)) == NULL) { /* not found */
+        np = (struct nlist2 *) malloc(sizeof(*np)); // malloc:
+        /*
+        void *malloc(size_t size)   <- void * means pointer of any type and is castable like the call above
+        */
+        if (np == NULL) // not exactly sure what this check is for
+          return NULL;
+        np->name = name;
+        np->defn = defn;
+        hashval = hash2(name);
+        np->next = hashtab2[hashval];
+        hashtab2[hashval] = np;
+    } else
+        np->defn = defn;
+    return np;
+}
+
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -60,7 +111,7 @@ int main()
             ++curi;
           }
           fprintf(stderr, "were out! %d\n", sum);
-          install(sum,sum);
+          install2(sum,sum);
           if (lookup2(sum) != NULL) {
             fprintf(stderr, "repeated %d!", sum);
             strcpy(a.smiley,":(");
@@ -99,53 +150,3 @@ int main()
 }
 
 // linux provides hcreate hsearch hdestroy! hash search and hashcreate
-
-struct nlist2 { /* table entry: */
-    struct nlist2 *next; /* next entry in chain */
-    int name; /* defined name */
-    int defn; /* replacement text */
-};
-
-#define HASHSIZE 101
-struct nlist2 *hashtab2[HASHSIZE];
-struct nlist2 *orig[HASHSIZE];
-
-unsigned hash2(int i) {
-  return i%HASHSIZE;
-}
-
-void reset_ht() {
-  memcpy( hashtab2, orig, sizeof hashtab2 );
-}
-
-/* lookup: look for s in hashtab */
-struct nlist2 *lookup2(int i)
-{
-    struct nlist2 *np;
-    for (np = hashtab2[hash2(i)]; np != NULL; np = np->next) // NULL in C is null pointer
-        if (i == np->name)
-          return np; /* found */
-    // printf("not found!");
-    return NULL; /* not found */
-}
-
-struct nlist2 *install2(int name, int defn)
-{
-    struct nlist2 *np;
-    unsigned hashval;
-    if ((np = lookup2(name)) == NULL) { /* not found */
-        np = (struct nlist2 *) malloc(sizeof(*np)); // malloc:
-        /*
-        void *malloc(size_t size)   <- void * means pointer of any type and is castable like the call above
-        */
-        if (np == NULL) // not exactly sure what this check is for
-          return NULL;
-        np->name = name;
-        np->defn = defn;
-        hashval = hash2(name);
-        np->next = hashtab2[hashval];
-        hashtab2[hashval] = np;
-    } else
-        np->defn = defn;
-    return np;
-}
