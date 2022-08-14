291728b301065b7fbd3c43bb3cc65b3d61a45552
Raskolnikov <jerry.tann@endress.com>
2022-08-14 12:40:25 +0800

    📝 not a good hash, doesn't support hash overflow

--- Utils/inthashset.c
+++ Utils/inthashset.c
@@ -8,17 +8,18 @@
 struct DataItem {
    int data;
    int key;
-};
+}; // struct defined
 
-struct DataItem* hashArray[SIZE];
-struct DataItem* dummyItem;
-struct DataItem* item;
+struct DataItem* hashArray[SIZE]; // struct 1, hashArray (pointer)
+struct DataItem* dummyItem;  // struct 2
+struct DataItem* item;  // struct 3,
 
 int hashCode(int key) {
-   return key % SIZE;
+   return key % SIZE;  // hash function, simplest
 }
 
-struct DataItem *search(int key) {
+// struct keyword necessary
+struct DataItem *search(int key) { // return pointer to struct, see https://stackoverflow.com/questions/2702928/returning-a-struct-pointer
    //get the hash
    int hashIndex = hashCode(key);
 
@@ -40,7 +41,7 @@ struct DataItem *search(int key) {
 
 void insert(int key,int data) {
 
-   struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem));
+   struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem)); // malloc, up to caller to free
    item->data = data;
    item->key = key;
 
@@ -108,6 +109,18 @@ int main() {
    insert(1, 20);
    insert(2, 70);
    insert(42, 80);
+   insert(62, 62);
+   insert(82,82);
+   insert(102,102);
+   insert(122,122);
+   insert(142,142);
+   insert(162,162);
+   insert(182,182);
+   insert(202,202);
+   insert(222,222);
+   insert(242,242);
+   insert(262,262);
+   insert(282,282);
    insert(4, 25);
    insert(12, 44);
    insert(14, 32);
@@ -117,7 +130,10 @@ int main() {
 
    display();
    item = search(37);
-
+   struct DataItem* item2 = search(202);
+   if(item2 != NULL) {
+     printf("element found: %d\n", item2->data);
+   }
    if(item != NULL) {
       printf("Element found: %d\n", item->data);
    } else {
