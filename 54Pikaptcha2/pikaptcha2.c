8e981538de03ecd00d3c8c619ca6d8afca0564f1
Raskolnikov <jerry.tann@endress.com>
2022-08-23 11:59:05 +0800

    ✨ TIL: c gotcha - order of initialization

--- 54Pikaptcha2/pikaptcha2.c
+++ 54Pikaptcha2/pikaptcha2.c
@@ -52,10 +52,24 @@ Output
 #include <string.h>
 #include <stdbool.h>
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+
+/*
+global vars, quick lesson
+static vs extern
+static - used only by program
+extern - can be used in other c programs
+
+// (both done outside fn)
+extern int num1;
+static int num2;
+*/
+
+static int WIDTH;
+static int HEIGHT;
+
+int getNextDir(int cury, int curx, int curd) {
+
+}
 
 int main()
 {
@@ -64,6 +78,8 @@ int main()
     int startx = 0;
     int starty = 0;
     int curd = 0;
+    WIDTH = width;
+    HEIGHT = height;
     int counts[height][width]; // use -1 for #
     scanf("%d%d", &width, &height);
     char grid[height][256];
@@ -110,7 +126,37 @@ int main()
 
     fprintf(stderr, "side is %s\n", side);
     fprintf(stderr, "starting at %d %d with direction %d\n", starty,startx,curd);
-
+    int px = 0;
+    int py = 0; // for some reason, doing this late leads to timeout
+    switch (curd) {
+      case 0:
+        if ((px+1) < width) {
+          px+=1;
+        } else {
+          if (side == "L") {
+            // R to U
+            if ((py - 1) > 0) {
+              py -= 1;
+            } else {
+              // must turn again (getting too nested)
+            }
+            curd = 1;
+          } else {
+            // R to D
+            if ((py + 1) < width) {
+              py += 1;
+            } else {
+            }
+            curd = 2;
+          }
+        }
+        break;
+      case 1:
+      case 2:
+      default:
+        break;
+    }
+    fprintf(stderr, "width and height are %d, %d\n", WIDTH, HEIGHT);
     for (int i = 0; i < height; i++) {
 
         // Write an action using printf(). DON'T FORGET THE TRAILING \n
