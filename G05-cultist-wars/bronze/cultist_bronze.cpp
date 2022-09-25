261ebf3bbef4b6ba1a7a5e9ff15d8135b83d3590
Raskolnikov <jerry.tann@endress.com>
2022-09-25 19:34:13 +0800

    ✨ make both branches of shooting match in logic

--- G05-cultist-wars/bronze/cultist_bronze.cpp
+++ G05-cultist-wars/bronze/cultist_bronze.cpp
@@ -76,8 +76,8 @@ bool path_has_obstacle(Unit u1, Unit u2) {
       (((double)right.y - (double)left.y) / ((double)right.x - (double)left.x));
   if (abs(slope) <= 1) {
     for (int x = 1; x < (right.x - left.x); ++x) {
-      int y = (int)floor(slope * (double)x) + left.y;
-      if (g_lines[y][x+left.x] ==
+      int dy = (int)floor(slope * (double)x);
+      if (g_lines[dy + left.y][x+left.x] ==
           'x') {
         return true;
       }
