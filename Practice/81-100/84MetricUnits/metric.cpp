f059622d0dd03412bf4a2b603401799c0244d390
Raskolnikov <jerry.tann@endress.com>
2022-11-28 13:45:25 +0800

    ✅ pass tests, cleanup, prepare for submission

--- Practice/81-100/84MetricUnits/metric.cpp
+++ Practice/81-100/84MetricUnits/metric.cpp
@@ -108,22 +108,16 @@ class UnitCalculator {
       }
     }
 
-    double add() {
-      cerr << "n 0 is " << n[0] << endl;
-            cerr << "n 1 is " << n[1] << endl;
-      cerr << "u 0 is " << u[0] << endl;
-            cerr << "u 1 is " << u[1] << endl;
+    string add() {
       int unit1mag = lookup[u[0]];
       int unit2mag = lookup[u[1]];
       double f = n[0] * lookup[u[0]];
       double s = n[1] * lookup[u[1]];
-      cerr << "f is " << f << endl;
-      cerr << "s is " << s << endl;
       double sum = f+s;
-      cerr << "unprocessedsum " << sum << endl;
-      sum /= (unit1mag <= unit2mag) ? lookup[u[0]] : lookup[u[1]];
+      string unitout = (unit1mag <= unit2mag) ? u[0] : u[1];
+      sum /= lookup[unitout];
 
-      return sum;
+      cout << sum << unitout << endl;;
     }
 };
 
@@ -132,7 +126,6 @@ int main()
     string expression;
     getline(cin, expression); // the expression to calculate
     UnitCalculator u = UnitCalculator(expression);
-    double d = u.add();
-    cerr << "sum is " << d << endl;
-    cout << "1cm" << endl;
+    u.add();
+
 }
