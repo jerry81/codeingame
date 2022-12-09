c1386a5fea14e10b410effa2df48d075c7a858b4
Raskolnikov <jerry.tann@endress.com>
2022-12-09 17:22:23 +0800

    ✨ find breakpoint or 0 if none

---
+++ Practice/81-100/93NextGrowingNumber/nextgrowing.lua
@@ -0,0 +1,46 @@
+--[[
+	Goal
+A growing number is a number where each digit, from left to right, is greater than or equal to the previous one.
+
+Given a number n, which may or may not be a growing number, you have to find the first growing number > n.
+
+Example:
+With n = 19, the first growing number greater than n is 22, because both 20 and 21 have a second digit that is smaller than the first one.
+
+Note: This problem can be solved without converting n to a number.
+Input
+line 1: a string representing the number n
+Output
+line 1: the first growing number > n
+Constraints
+n < 10^19
+Example
+Input
+19
+Output
+22
+]]--
+
+-- Auto-generated code below aims at helping you parse
+-- the standard input according to the problem statement.
+
+n = io.read()
+io.stderr:write("type is "..type(n).."\n")
+function findbreakpoint(n)
+    previous = 0
+	for i=1,#n do
+		num=tonumber(string.sub(n,i,i))
+        if num < previous then
+            return i
+        end
+        previous = num
+	end
+    return 0
+end
+
+breakpoint = findbreakpoint(n)
+io.stderr:write("breakpoint is "..breakpoint.."\n")
+-- Write an answer using print()
+-- To debug: io.stderr:write("Debug message\n")
+
+print("answer")
