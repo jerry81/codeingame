a02f6e60116e0b8902586696ffc9274e210e5ab0
Raskolnikov <jerry.tann@endress.com>
2022-12-25 14:05:38 +0800

    ✨ build map

--- Practice/Medium/110Scrabble/scrabble.cpp
+++ Practice/Medium/110Scrabble/scrabble.cpp
@@ -1,16 +1,23 @@
 /*
 
 The Goal
-When playing Scrabble©, each player draws 7 letters and must find a word that scores the most points using these letters.
+When playing Scrabble©, each player draws 7 letters and must find a word that
+scores the most points using these letters.
 
-A player doesn't necessarily have to make a 7-letter word; the word can be shorter. The only constraint is that the word must be made using the 7 letters which the player has drawn.
+A player doesn't necessarily have to make a 7-letter word; the word can be
+shorter. The only constraint is that the word must be made using the 7 letters
+which the player has drawn.
 
-For example, with the letters  etaenhs, some possible words are: ethane, hates, sane, ant.
+For example, with the letters  etaenhs, some possible words are: ethane, hates,
+sane, ant.
 
-Your objective is to find the word that scores the most points using the available letters (1 to 7 letters).
+Your objective is to find the word that scores the most points using the
+available letters (1 to 7 letters).
 
  Rules
-In Scrabble©, each letter is weighted with a score depending on how difficult it is to place that letter in a word. You will see below a table showing the points corresponding to each letter:
+In Scrabble©, each letter is weighted with a score depending on how difficult it
+is to place that letter in a word. You will see below a table showing the points
+corresponding to each letter:
 
 
 Letters	Points
@@ -23,10 +30,15 @@ j, x	8
 q, z	10
 The word banjo earns you 3 + 1 + 1 + 8 + 1 = 14 points.
 
-A dictionary of authorized words is provided as input for the program. The program must find the word in the dictionary which wins the most points for the seven given letters (a letter can only be used once). If two words win the same number of points, then the word which appears first in the order of the given dictionary should be chosen.
+A dictionary of authorized words is provided as input for the program. The
+program must find the word in the dictionary which wins the most points for the
+seven given letters (a letter can only be used once). If two words win the same
+number of points, then the word which appears first in the order of the given
+dictionary should be chosen.
 
 
-All words will only be composed of alphabetical characters in lower case. There will always be at least one possible word.
+All words will only be composed of alphabetical characters in lower case. There
+will always be at least one possible word.
 
  Game Input
 Input
@@ -37,11 +49,10 @@ N following lines: the words in the dictionary. One word per line.
 Last line: the 7 letters available.
 
 Output
-The word that scores the most points using the available letters (1 to 7 letters). The word must belong to the dictionary. Each letter must be used at most once in the solution. There is always a solution.
-Constraints
-0 < N < 100000
-Words in the dictionary have a maximum length of 30 characters.
-Example
+The word that scores the most points using the available letters (1 to 7
+letters). The word must belong to the dictionary. Each letter must be used at
+most once in the solution. There is always a solution. Constraints 0 < N <
+100000 Words in the dictionary have a maximum length of 30 characters. Example
 Input
 5
 because
@@ -55,31 +66,58 @@ which
 
 */
 
+#include <algorithm>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+unordered_map<char, int> lookup;
 
-int main()
-{
-    int n;
-    cin >> n; cin.ignore();
-    for (int i = 0; i < n; i++) {
-        string w;
-        getline(cin, w);
-    }
-    string letters;
-    getline(cin, letters);
+void build_lookup() {
+  lookup['e'] = 1;
+  lookup['a'] = 1;
+  lookup['i'] = 1;
+  lookup['o'] = 1;
+  lookup['n'] = 1;
+  lookup['r'] = 1;
+  lookup['t'] = 1;
+  lookup['l'] = 1;
+  lookup['s'] = 1;
+  lookup['u'] = 1;
+  lookup['d'] = 2;
+  lookup['g'] = 2;
+  lookup['b'] = 3;
+  lookup['c'] = 3;
+  lookup['m'] = 3;
+  lookup['p'] = 3;
+  lookup['f'] = 4;
+  lookup['h'] = 4;
+  lookup['v'] = 4;
+  lookup['w'] = 4;
+  lookup['y'] = 4;
+  lookup['k'] = 5;
+  lookup['j'] = 8;
+  lookup['x'] = 8;
+  lookup['q'] = 10;
+  lookup['z'] = 10;
+}
 
-    // Write an answer using cout. DON'T FORGET THE "<< endl"
-    // To debug: cerr << "Debug messages..." << endl;
+int main() {
+  int n;
+  cin >> n;
+  cin.ignore();
+  for (int i = 0; i < n; i++) {
+    string w;
+    getline(cin, w);
+  }
+  string letters;
+  getline(cin, letters);
 
-    cout << "invalid word" << endl;
+  // Write an answer using cout. DON'T FORGET THE "<< endl"
+  // To debug: cerr << "Debug messages..." << endl;
+
+  cout << "invalid word" << endl;
 }
