1d3466ecf477b7d260d50487189c6524f6579711
Raskolnikov <jerry.tann@endress.com>
2023-01-17 16:45:59 +0800

    ✨ simple drafting

--- AI/G07-Code-Magic/code-magic-wood3.cpp
+++ AI/G07-Code-Magic/code-magic-wood3.cpp
@@ -3,48 +3,50 @@
 
 This is a league based challenge.
 
-Wood leagues should be considered as a tutorial which lets players discover the different rules of the game.
-In Bronze league, all rules will be unlocked and the real challenge will begin.
- 	The Goal
-Draft a deck of cards, battle an opponent with those cards and reduce their Health Points (HP) from 30 to 0.
- 	Rules
-This game is a two-player card game which is played in two phases: the Draft phase and the Battle phase.
-During the Draft phase, both players must create a deck of 30 cards.
-Once the Draft phase is over, both decks are shuffled.
-During the Battle, the board is divided in two parts: each player plays cards from their hand on their side of the board.
-To reduce the health points of an opponent, the player must make use of cards to deal damage.
- 	Draft Phase
-For 30 turns, both players are given a choice between 3 different cards. Players select the card they want to add to their deck by using the PICK command followed by 0, 1 or 2.
-By default, the PASS command will pick the first card.
-Both players can select the same card, they will each receive a copy.
- 	Battle Phase
-Card Draw
-First player starts with 4 cards in hand whereas the second player starts with 5.
-Each turn, the active player draws one additional card from their deck.
-Both players possess runes which have an impact on card draw. However, runes should be ignored until the Bronze league.
+Wood leagues should be considered as a tutorial which lets players discover the
+different rules of the game. In Bronze league, all rules will be unlocked and
+the real challenge will begin. The Goal Draft a deck of cards, battle an
+opponent with those cards and reduce their Health Points (HP) from 30 to 0.
+        Rules
+This game is a two-player card game which is played in two phases: the Draft
+phase and the Battle phase. During the Draft phase, both players must create a
+deck of 30 cards. Once the Draft phase is over, both decks are shuffled. During
+the Battle, the board is divided in two parts: each player plays cards from
+their hand on their side of the board. To reduce the health points of an
+opponent, the player must make use of cards to deal damage. Draft Phase For 30
+turns, both players are given a choice between 3 different cards. Players select
+the card they want to add to their deck by using the PICK command followed by 0,
+1 or 2. By default, the PASS command will pick the first card. Both players can
+select the same card, they will each receive a copy. Battle Phase Card Draw
+First player starts with 4 cards in hand whereas the second player starts
+with 5. Each turn, the active player draws one additional card from their deck.
+Both players possess runes which have an impact on card draw. However, runes
+should be ignored until the Bronze league.
 
 Mana
 Mana is necessary to play cards from the hand.
 The first player starts with 1 max mana, the second with 2 max mana.
 Each player can spend as much mana per turn as they have max mana.
-The second player receives his +1 max mana bonus until he spends all his mana during a turn.
-Each turn, the active player is granted one additional max mana, unless they already have 12 max mana (13 for the second player who didn't spend his bonus).
- 	Card TypesIn this league, there is only one type of card: Creatures.
+The second player receives his +1 max mana bonus until he spends all his mana
+during a turn. Each turn, the active player is granted one additional max mana,
+unless they already have 12 max mana (13 for the second player who didn't spend
+his bonus). Card TypesIn this league, there is only one type of card: Creatures.
 
 Creatures
 Placing a creature card from the hand to the board is called summoning.
-A player summons creatures to their side of the board by paying their cost in mana. They are used to attack the opponent and also serve as a defense against the creatures of the opposing player.
-Creatures have a cost in mana, attack points and defense points.
-By default, creatures can't attack the turn they are summoned. They can attack once per turn only.
-When a creature attacks another one, they both deal damage equals to their attack to the defense of the other creature. When a creature attacks the opponent, it deals damage equals to its attack to the HP of the opponent.
-Creatures are removed from play when their defense reaches 0 or less.
- 	Gameplay
-Possible Actions
-SUMMON id to summon the creature id from your hand.
-ATTACK id1 id2 to attack creature id2 with creature id1.
-ATTACK id -1 to attack the opponent directly with creature id.
-PASS to do nothing this turn.
-A player can do any number of valid actions during one turn. Actions must be separated by a semi-colon ;.
+A player summons creatures to their side of the board by paying their cost in
+mana. They are used to attack the opponent and also serve as a defense against
+the creatures of the opposing player. Creatures have a cost in mana, attack
+points and defense points. By default, creatures can't attack the turn they are
+summoned. They can attack once per turn only. When a creature attacks another
+one, they both deal damage equals to their attack to the defense of the other
+creature. When a creature attacks the opponent, it deals damage equals to its
+attack to the HP of the opponent. Creatures are removed from play when their
+defense reaches 0 or less. Gameplay Possible Actions SUMMON id to summon the
+creature id from your hand. ATTACK id1 id2 to attack creature id2 with creature
+id1. ATTACK id -1 to attack the opponent directly with creature id. PASS to do
+nothing this turn. A player can do any number of valid actions during one turn.
+Actions must be separated by a semi-colon ;.
 
 Game End
 The game is over once any player reaches 0 or less HP.
@@ -54,34 +56,39 @@ Loss Conditions
 Your HP gets reduced to 0 or less.
 You do not respond in time or output an unrecognized command.
 
- 	Advanced Details
-Advanced details will be available in the Bronze league, as well as the game's source code for reference.
+        Advanced Details
+Advanced details will be available in the Bronze league, as well as the game's
+source code for reference.
 
- 	Game Input
+        Game Input
 Input for one game turn
-First 2 lines: for each player, playerHealth, playerMana, playerDeck and playerRune:
-Integer playerHealth: the remaining HP of the player.
-Integer playerMana: the current maximum mana of the player.
-Integer playerDeck: the number of cards in the player's deck. During the Draft phase, the second player has less card in his deck than his opponent.
-Integer playerRune: to be ignored in this league
-Integer playerDraw: to be ignored in this league.
-The player's input comes first, the opponent's input comes second.
+First 2 lines: for each player, playerHealth, playerMana, playerDeck and
+playerRune: Integer playerHealth: the remaining HP of the player. Integer
+playerMana: the current maximum mana of the player. Integer playerDeck: the
+number of cards in the player's deck. During the Draft phase, the second player
+has less card in his deck than his opponent. Integer playerRune: to be ignored
+in this league Integer playerDraw: to be ignored in this league. The player's
+input comes first, the opponent's input comes second.
 
 During the Draft phase, playerMana is always 0.
 
 Next line:
-Integer opponentHand, the total number of cards in the opponent's hand. These cards are hidden until they're played.
-Integer opponentActions, the number of actions performed by the opponent during his last turn.
-Next opponentActions lines: for each opponent's action, string cardNumberAndAction containing the cardNumber of the played card, followed by a space, followed by the action associated with this card (see Possible Actions section).
+Integer opponentHand, the total number of cards in the opponent's hand. These
+cards are hidden until they're played. Integer opponentActions, the number of
+actions performed by the opponent during his last turn. Next opponentActions
+lines: for each opponent's action, string cardNumberAndAction containing the
+cardNumber of the played card, followed by a space, followed by the action
+associated with this card (see Possible Actions section).
 
-Next line: Integer cardCount: during the Battle phase, the total number of cards on the board and in the player's hand. During the Draft phase, always 3.
+Next line: Integer cardCount: during the Battle phase, the total number of cards
+on the board and in the player's hand. During the Draft phase, always 3.
 
-Next cardCount lines: for each card, cardNumber, instanceId, location, cardType, cost, attack, defense, abilities, myhealthChange, opponentHealthChange and cardDraw:
-Integer cardNumber: the identifier of a card (see complete list).
-Integer instanceId: the identifier representing the instance of the card (there can be multiple instances of the same card in a game).
-Integer location, during the Battle phase:
-0: in the player's hand
-1: on the player's side of the board
+Next cardCount lines: for each card, cardNumber, instanceId, location, cardType,
+cost, attack, defense, abilities, myhealthChange, opponentHealthChange and
+cardDraw: Integer cardNumber: the identifier of a card (see complete list).
+Integer instanceId: the identifier representing the instance of the card (there
+can be multiple instances of the same card in a game). Integer location, during
+the Battle phase: 0: in the player's hand 1: on the player's side of the board
 -1: on the opponent's side of the board
 Always 0 during the Draft phase.
 Integer cardType: always 0 for this league.
@@ -93,16 +100,15 @@ Integer myHealthChange: to be ignored in this league.
 Integer opponentHealthChange: to be ignored in this league.
 Integer cardDraw: to be ignored in this league.
 Output for one game turn of the Draft
-PICK nb where nb equals 0, 1 or 2 to choose one of the three proposed cards to add to your deck.
-PASS to do nothing (picks the 1st card by default).
-Output for one game turn of the Card Battle
-The available actions are:
-SUMMON id to summon the creature of instanceId id from the player's hand.
-ATTACK idAttacker idTarget to attack an opposing creature or opposing player of instanceId idTarget with a creature on the board of instanceId idAttacker.
-idTarget can be the "no-creature" identifier -1. It is used to attack the opponent directly.
-PASS to do nothing.
-Players may use several actions by using a semi-colon ;.
-Players may append text to each of their actions, it will be displayed in the viewer.
+PICK nb where nb equals 0, 1 or 2 to choose one of the three proposed cards to
+add to your deck. PASS to do nothing (picks the 1st card by default). Output for
+one game turn of the Card Battle The available actions are: SUMMON id to summon
+the creature of instanceId id from the player's hand. ATTACK idAttacker idTarget
+to attack an opposing creature or opposing player of instanceId idTarget with a
+creature on the board of instanceId idAttacker. idTarget can be the
+"no-creature" identifier -1. It is used to attack the opponent directly. PASS to
+do nothing. Players may use several actions by using a semi-colon ;. Players may
+append text to each of their actions, it will be displayed in the viewer.
 
 Example: SUMMON 3;ATTACK 4 5 yolo; ATTACK 8 -1 no fear.
 Constraints
@@ -124,18 +130,18 @@ In Bronze, more abilities for creatures!
 
 */
 
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 struct GameMove {
-  int type; // 0 SUMMON, 1 ATTACK, 2 PICK, other PASS
+  int type;  // 0 SUMMON, 1 ATTACK, 2 PICK, other PASS
   int id;
   int id2;
-  GameMove(int i1 = -1,int i2=-1, int m=2): id(i1), id2(i2), type(m){};
+  GameMove(int i1 = -1, int i2 = -1, int m = 2) : id(i1), id2(i2), type(m){};
   string stringify() {
     switch (type) {
       case 0:
@@ -153,11 +159,16 @@ struct GameMove {
 struct Card {
   int id;
   int instance_id;
-  int location; // 0 player hand / draft, 1 player side, 2 opponent side
+  int location;  // 0 player hand / draft, 1 player side, 2 opponent side
   int cost;
   int attack;
   int defense;
-  Card(int i, int ii, int loc, int c, int a, int d):id(i), instance_id(ii), location(loc), cost(c), attack(a), defense(d){};
+  Card(int i = -1, int ii = -1, int loc = -1, int c = -1, int a = -1,
+       int d = -1)
+      : id(i), instance_id(ii), location(loc), cost(c), attack(a), defense(d){};
+  void print() {
+    cerr << "id " << id << " attack " << attack << " defense " << defense << endl;
+  }
 };
 
 struct Player {
@@ -165,46 +176,68 @@ struct Player {
   int mana;
   bool me;
   int deck_size;
-  Player(int h = 0, int m = 0, int d = 0, bool me = false) : health(h), mana(m), deck_size(d), me(me) {};
+  Player(int h = 0, int m = 0, int d = 0, bool me = false)
+      : health(h), mana(m), deck_size(d), me(me){};
   void print() {
-    cerr << "my health " << health << " mana " << mana << " deck size " << deck_size << endl;
+    cerr << "my health " << health << " mana " << mana << " deck size "
+         << deck_size << endl;
   };
 };
 
 class Game {
   Player me;
+  vector<Card> drafting;
 
-  public:
-    void setMe(int h, int m, int d) {
-      me = Player(h, m, d, true);
-    };
-    bool draft() {
-      return me.mana == 0;
-    };
-    void print() {
-      cerr << "me" << endl;
-      me.print();
-      string phase = (draft() ? "draft" : "battle");
-      cerr << "phase: " << phase << endl;
-    };
-
+ public:
+  void reset() {
+    drafting.clear();
+  }
+  void addDraft(Card c) {
+    drafting.push_back(c);
+  }
+  void setMe(int h, int m, int d) { me = Player(h, m, d, true); };
+  bool draft() { return me.mana == 0; };
+  void print() {
+    cerr << "me" << endl;
+    me.print();
+    string phase = (draft() ? "draft" : "battle");
+    cerr << "phase: " << phase << endl;
+  };
+  int pickCard() {
+    int maxVal = 0;
+    int maxIdx = 0;
+    for (int i = 0; i < 3; ++i) {
+     Card c = drafting[i];
+     c.print();
+      int val = c.attack + c.defense;
+      if (val > maxVal) {
+        maxVal = val;
+        maxIdx = i;
+      }
+    }
+    return maxIdx;
+  };
 };
 
 Game g;
+
 
 int main()
 {
 
     // game loop
     while (1) {
+      g.reset();
         for (int i = 0; i < 2; i++) {
             int player_health;
             int player_mana;
             int player_deck;
             int player_rune;
             int player_draw;
-            g.setMe(player_health, player_mana, player_deck);
             cin >> player_health >> player_mana >> player_deck >> player_rune >> player_draw; cin.ignore();
+            if (i == 0) {
+              g.setMe(player_health, player_mana, player_deck);
+            }
         }
         int opponent_hand;
         int opponent_actions;
@@ -215,6 +248,7 @@ int main()
         }
         int card_count;
         cin >> card_count; cin.ignore();
+
         for (int i = 0; i < card_count; i++) {
             int card_number;
             int instance_id;
@@ -228,13 +262,24 @@ int main()
             int opponent_health_change;
             int card_draw;
             cin >> card_number >> instance_id >> location >> card_type >> cost >> attack >> defense >> abilities >> my_health_change >> opponent_health_change >> card_draw; cin.ignore();
+            Card c = Card(card_number,instance_id, location, cost, attack, defense);
+            c.print();
+            g.addDraft(c);
         }
 
-        g.print();
+       int choice = g.pickCard();
+       cerr <<  "choice" << choice << endl;
+       GameMove gm;
+       if (g.draft()) {
+         gm.id = choice;
+         gm.type = 2;
+       } else {
+         gm.type = 3;
+       }
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
 
-        cout << "PASS" << endl;
+        cout << gm.stringify() << endl;
     }
 }
