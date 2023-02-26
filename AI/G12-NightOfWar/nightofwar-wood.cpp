4dd1b9a4f96aa803b62f2d9fbb11fe0b16757ffa
Raskolnikov <jerry.tann@endress.com>
2023-02-26 12:18:30 +0800

    🚧 setup

--- AI/G12-NightOfWar/nightofwar-wood.cpp
+++ AI/G12-NightOfWar/nightofwar-wood.cpp
@@ -1,7 +1,7 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -10,44 +10,96 @@ using namespace std;
  * -Kill your enemy soldiers or Have more bucks than your enemy at end of game
  **/
 
-int main()
-{
-    int my_id; // Your unique player Id
-    cin >> my_id; cin.ignore();
-    int map_size; // the size of map MapSize*MapSize
-    cin >> map_size; cin.ignore();
+const int UP = 0;
+const int LEFT = 1;
+const int DOWN = 2;
+const int RIGHT = 3;
 
-    // game loop
-    while (1) {
-        int my_bucks; // Your Money
-        cin >> my_bucks; cin.ignore();
-        int opp_bucks; // Opponent Money
-        cin >> opp_bucks; cin.ignore();
-        for (int i = 0; i < map_size; i++) {
-            for (int j = 0; j < map_size; j++) {
-                int block_owner; // The playerId of this box owned player
-                int x; // This block's position x
-                int y; // This block's position y
-                cin >> block_owner >> x >> y; cin.ignore();
-            }
-        }
-        int active_soldier_count; // Total no. of active soldier in the game
-        cin >> active_soldier_count; cin.ignore();
-        for (int i = 0; i < active_soldier_count; i++) {
-            int owner_id; // owner of the soldier
-            int x; // This soldier's position x
-            int y; // This soldier's position y
-            int soldier_id; // The unique identifier of soldier
-            int level; // Level of the soldier ignore for first league
-            int direction; // The side where the soldier is facing 0 = UP, 1 = LEFT , 2 = DOWN, 3 = RIGHT
-            cin >> owner_id >> x >> y >> soldier_id >> level >> direction; cin.ignore();
-        }
+struct Point {
+  int x = 0;
+  int y = 0;
+  string hash() { return std::to_string(x) + "," + std::to_string(y); }
+  Point(int x = -1, int y = -1)
+      : x(x), y(y){};  // init list syntax Constructor followed by :
+                       // field(param), field(param)
+};
 
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
+struct Soldier {
+  int id = -1;
+  int owner = -1;
+  int level;
+  int direction = UP;
+  Point p;
+};
+struct Block {
+  Point p;
+  int owner = 0;
+};
+class Game {
+  int myId = 0;
+  int mapSize = 0;
 
+  vector<vector<Block>> map;
 
-        // print any of actions - WAIT | MOVE <soldierId> <direction> | ATTACK <soldierID> <soldierId to attack on> | LATER > UPGRADE <id> | DEGRADE <opponent id> | SUICIDE <id>
-        cout << "WAIT" << endl;
+ public:
+  Game(int myId = 0, int mapSize = 0) : myId(myId), mapSize(mapSize){};
+
+  void reset() { map.clear(); };
+
+  void addMapLine(vector<Block> line) { map.push_back(line); };
+};
+
+int main() {
+  int my_id;  // Your unique player Id
+  cin >> my_id;
+  cin.ignore();
+  int map_size;  // the size of map MapSize*MapSize
+  cin >> map_size;
+  cin.ignore();
+  Game g = Game(my_id, map_size);
+
+  // game loop
+  while (1) {
+    g.reset();
+    int my_bucks;  // Your Money
+    cin >> my_bucks;
+    cin.ignore();
+    int opp_bucks;  // Opponent Money
+    cin >> opp_bucks;
+    cin.ignore();
+    for (int i = 0; i < map_size; i++) {
+      vector<Block> line;
+      for (int j = 0; j < map_size; j++) {
+        int block_owner;  // The playerId of this box owned player
+        int x;            // This block's position x
+        int y;            // This block's position y
+        Block b;
+        cin >> block_owner >> x >> y;
+        b.owner = block_owner;
+        Point p = Point(x, y);
+        b.p = p;
+        line.push_back(b);
+        cin.ignore();
+      }
     }
+    int active_soldier_count;  // Total no. of active soldier in the game
+    cin >> active_soldier_count;
+    cin.ignore();
+    for (int i = 0; i < active_soldier_count; i++) {
+      int owner_id;    // owner of the soldier
+      int x;           // This soldier's position x
+      int y;           // This soldier's position y
+      int soldier_id;  // The unique identifier of soldier
+      int level;       // Level of the soldier ignore for first league
+      int direction;  // The side where the soldier is facing 0 = UP, 1 = LEFT ,
+                      // 2 = DOWN, 3 = RIGHT
+      cin >> owner_id >> x >> y >> soldier_id >> level >> direction;
+      cin.ignore();
+    }
+
+    // print any of actions - WAIT | MOVE <soldierId> <direction> | ATTACK
+    // <soldierID> <soldierId to attack on> | LATER > UPGRADE <id> | DEGRADE
+    // <opponent id> | SUICIDE <id>
+    cout << "WAIT" << endl;
+  }
 }
