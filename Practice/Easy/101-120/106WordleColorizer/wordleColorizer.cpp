bca08f07ded8dc23974921e5ab452965af567b02
Raskolnikov <jerry.tann@endress.com>
2023-05-05 15:07:20 +0800

    ✨ first attempt

--- Practice/Easy/101-120/106WordleColorizer/wordleColorizer.cpp
+++ Practice/Easy/101-120/106WordleColorizer/wordleColorizer.cpp
@@ -15,19 +15,31 @@ using namespace std;
 
 int main()
 {
-    unordered_map<char, int> position_lookup;
-    unordered_map<char, bool> presence_lookup;
+    unordered_map<char, int> presence_lookup;
     string answer;
     getline(cin, answer);
+    for (char c: answer) {
+      presence_lookup[c] = true;
+    }
     int n;
     cin >> n; cin.ignore();
     for (int i = 0; i < n; i++) {
+        unordered_map<char, int> lookup = presence_lookup;
         string attempt;
         getline(cin, attempt);
+        string ans = "";
+        for (int i = 0; i < attempt.size(); ++i) {
+          if (answer[i] == attempt[i]) {
+            ans+='#';
+          } else if (lookup[attempt[i]] > 0) {
+            ans+= 'O';
+            lookup[attempt[i]]--;
+          } else {
+            ans+= 'X';
+          }
+        }
+        cout << ans << endl;
     }
-
-
-    cout << "OXOXO" << endl;
 }
 
 // 2 maps, position, presence
