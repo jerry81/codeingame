89bacca44067a1414882fd896b875f3964de5c82
Raskolnikov <jerry.tann@endress.com>
2023-05-27 13:32:52 +0800

    🚧 try new strat - closest

--- AI/G13-Spring2023/wood2/sping2023.cpp
+++ AI/G13-Spring2023/wood2/sping2023.cpp
@@ -2,15 +2,38 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 vector<vector<int>> neighbors;
+vector<int> resourceMap;
+int _size;
+
+int bfs(int start) {
+  vector<bool> visited(_size, false);
+  queue<int> nn;
+  visited[start] = true;
+  nn.push(start);
+  while (!nn.empty()) {
+    int cur = nn.front();
+    nn.pop();
+
+    if (resourceMap[cur] > 0) return cur;
+
+    for (int n: neighbors[cur]) {
+      if (!visited[n]) nn.push(n);
+    }
+  }
+  return start;
+}
 
 int main() {
   int number_of_cells;  // amount of hexagonal cells in this map
   cin >> number_of_cells;
   cin.ignore();
-  neighbors.resize(number_of_cells, vector<int>());
+  _size = number_of_cells;
+  neighbors.resize(_size, vector<int>());
+  resourceMap.resize(_size, 0);
   for (int i = 0; i < number_of_cells; i++) {
     int type;               // 0 for empty, 1 for eggs, 2 for crystal
     int initial_resources;  // the initial amount of eggs/crystals on this cell
@@ -55,24 +78,24 @@ int main() {
     cin.ignore();
   }
 
-  vector<int> resourcesMap(number_of_cells, 0);
 
   // game loop
   while (1) {
-    for (int i = 0; i < number_of_cells; i++) {
+    for (int i = 0; i < _size; i++) {
       int resources;  // the current amount of eggs/crystals on this cell
       int my_ants;    // the amount of your ants on this cell
       int opp_ants;   // the amount of opponent ants on this cell
       cin >> resources >> my_ants >> opp_ants;
       cin.ignore();
-      if (resources>0) resourcesMap[i] = resources;
+      resourceMap[i] = resources;
     }
 
     // bfs to get closest resource to base.
+    int closest = bfs(baseIdx);
 
     // WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx>
     // <strength> | MESSAGE <text>
-    cout << "LINE " + to_string(maxIdx) + " " + to_string(baseIdx) + " 5;"
+    cout << "LINE " + to_string(closest) + " " + to_string(baseIdx) + " 5;"
          << endl;
   }
 }
