4f823481c12927226ce77ebb1cc2ab2ccb220f35
Raskolnikov <jerry.tann@endress.com>
2023-06-06 14:10:14 +0800

    🚧 handle verts, first attempt

--- Practice/Easy/101-120/107WordSearch/wordSearch.cpp
+++ Practice/Easy/101-120/107WordSearch/wordSearch.cpp
@@ -1,36 +1,31 @@
 /*
 
 Goal
-Word Search is a type of puzzle that asks the player to search for various words within a grid of letters.
-They can be vertical, horizonal, backwards, diagonal, etc.
-If you are unfamiliar with it, see banner image and/or https://en.wikipedia.org/wiki/Word_search
+Word Search is a type of puzzle that asks the player to search for various words
+within a grid of letters. They can be vertical, horizonal, backwards, diagonal,
+etc. If you are unfamiliar with it, see banner image and/or
+https://en.wikipedia.org/wiki/Word_search
 
-But as a computer geek, that doesn't interest you; you want to program a way to find those words instead.
+But as a computer geek, that doesn't interest you; you want to program a way to
+find those words instead.
 
-Input will be a square grid of size lines (each line is size letters long), followed by all the words to find.
+Input will be a square grid of size lines (each line is size letters long),
+followed by all the words to find.
 
-Output the same square grid but hide any letters that aren't included in those words.
+Output the same square grid but hide any letters that aren't included in those
+words.
 
 ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
-NOTE: If you enjoy this puzzle, see this similar one: https://www.codingame.com/training/medium/hidden-word
-Input
-Line 1: An integer size, denoting the number of lines of input
-Next size lines: A string ROW of that square grid
-Next line: A string clues of all the words to find, separated by spaces
-Output
-size lines: ROW with the unused letters hidden.
-In other words, all unused characters should be replaced by a space character.
-Trailing spaces should NOT be trimmed.
-Constraints
-• Each clue of clues has only letters (no spaces).
-• clues are given in capitalized case (but look for the all-caps version within the grid).
-Example
-Input
-10
-IPLUCJMCNY
-AAFIIVADZN
-TFUUSWGHWE
+NOTE: If you enjoy this puzzle, see this similar one:
+https://www.codingame.com/training/medium/hidden-word Input Line 1: An integer
+size, denoting the number of lines of input Next size lines: A string ROW of
+that square grid Next line: A string clues of all the words to find, separated
+by spaces Output size lines: ROW with the unused letters hidden. In other words,
+all unused characters should be replaced by a space character. Trailing spaces
+should NOT be trimmed. Constraints • Each clue of clues has only letters (no
+spaces). • clues are given in capitalized case (but look for the all-caps
+version within the grid). Example Input 10 IPLUCJMCNY AAFIIVADZN TFUUSWGHWE
 UBARTAGDOG
 YSAJIGIUGR
 DTEMRFEOKA
@@ -53,12 +48,12 @@ H        S
 
 */
 
+#include <algorithm>
+#include <cctype>
+#include <cstring>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <cstring>
-#include <cctype>
 
 using namespace std;
 
@@ -67,11 +62,10 @@ using namespace std;
  * the standard input according to the problem statement.
  **/
 
-
 vector<string> split(string str) {
   // Returns first token
   vector<string> ret;
-  char *token = strtok(str.data(), " ");
+  char* token = strtok(str.data(), " ");
 
   // Keep printing tokens while one of the
   // delimiters present in str[].
@@ -84,122 +78,123 @@ vector<string> split(string str) {
 }
 
 std::string toUpperCase(const std::string& str) {
-    std::string upperStr = str;
+  std::string upperStr = str;
 
-    std::transform(upperStr.begin(), upperStr.end(), upperStr.begin(), [](unsigned char c) {
-        return std::toupper(c);
-    });
+  std::transform(upperStr.begin(), upperStr.end(), upperStr.begin(),
+                 [](unsigned char c) { return std::toupper(c); });
 
-    return upperStr;
+  return upperStr;
 }
 
-int main()
-{
-    int size;
-    vector<string> rows;
-    vector<string> backwards;
-    cin >> size; cin.ignore();
-    for (int i = 0; i < size; i++) {
-        string row;
-        getline(cin, row);
-        rows.push_back(row);
-        reverse(row.begin(), row.end());
-        backwards.push_back(row);
-    }
+int main() {
+  int size;
+  vector<string> rows;
+  vector<string> backwards;
+  cin >> size;
+  cin.ignore();
+  for (int i = 0; i < size; i++) {
+    string row;
+    getline(cin, row);
+    rows.push_back(row);
+    reverse(row.begin(), row.end());
+    backwards.push_back(row);
+  }
 
+  string clues;
+  getline(cin, clues);
 
-    string clues;
-    getline(cin, clues);
+  vector<string> cluesspl = split(clues);
+  vector<string> cluesUpper;
+  for (string s : cluesspl) {
+    cluesUpper.push_back(toUpperCase(s));
+  }
 
-    vector<string> cluesspl = split(clues);
+  vector<vector<bool>> mat(size, vector<bool>(size, false));
 
-    vector<vector<bool>> mat(size, vector<bool>(size, false));
-
-    for (int i = 0; i < size; ++i) {
-      string s = rows[i];
-      string b = backwards[i];
-      for (string cl: cluesspl) {
-        cl = toUpperCase(cl);
-        int startpos = s.find(cl);
-        if (startpos != std::string::npos) {
-          cout << cl << " found at " << i << ", " << startpos << endl;
-          for (int x = 0; x < cl.size(); ++x) {
-            mat[i][x+startpos] = true;
-          }
-        }
-        startpos = b.find(cl);
-        if (startpos != std::string::npos) {
-          for (int x = 0; x < cl.size(); ++x) {
-            mat[i][size-startpos-x-1] = true;
-          }
+  for (int i = 0; i < size; ++i) {
+    string s = rows[i];
+    string b = backwards[i];
+    for (string cl : cluesUpper) {
+      int startpos = s.find(cl);
+      if (startpos != std::string::npos) {
+        for (int x = 0; x < cl.size(); ++x) {
+          mat[i][x + startpos] = true;
         }
       }
-    }
-
-
-
-    vector<string> vertical;
-    vector<string> verticalR;
-    for (int i = 0; i < size; ++i) {
-      string cur = "";
-      for (int j = 0; j < size; ++j) {
-        cur+=rows[j][i];
-      }
-      vertical.push_back(cur);
-      reverse(cur.begin(), cur.end());
-      verticalR.push_back(cur);
-    }
-
-    vector<string> diags;
-    vector<string> diagsR;
-    // 0 to size (l->r)
-    for (int i = 0; i < size; ++i) {
-      int x = i;
-      int y = 0;
-      string cur = "";
-      while (x < size && y < size) {
-        cur+=rows[y][x];
-        x++;
-        y++;
-      }
-      diags.push_back(cur);
-      reverse(cur.begin(), cur.end());
-      diagsR.push_back(cur);
-    }
-    for (int i = 1; i < size; ++i) {
-      int y = i;
-      int x = 0;
-      string cur = "";
-      while (x < size && y < size) {
-        cur+=rows[y][x];
-        x++;
-        y++;
-      }
-      diags.push_back(cur);
-      reverse(cur.begin(), cur.end());
-      diagsR.push_back(cur);
-    }
-
-
-    // 1 to size (t->b)
-
-
-
-    vector<string> rdiags;
-    vector<string> rdiagsR;
-
-    for (int i = 0; i < size; ++i) {
-       string cur = "";
-        for (int j = 0; j < size; ++j) {
-            cur+=(mat[i][j])? string(1,rows[i][j]) : " ";
+      startpos = b.find(cl);
+      if (startpos != std::string::npos) {
+        for (int x = 0; x < cl.size(); ++x) {
+          mat[i][size - startpos - x - 1] = true;
         }
-        cout << cur << endl;
+      }
     }
+  }
 
+  // vector<string> verticalR;
+  // vector<string> vertical;
+  for (int i = 0; i < size; ++i) {
+    string cur = "";
+    for (int j = 0; j < size; ++j) {
+      cur += rows[j][i];
+    }
+    // vertical.push_back(cur); // no longer needed?
+    for (string cl : cluesUpper) {
+      string reversed = cur;
+      reverse(reversed.begin(), reversed.end());
+      int startpos = cur.find(cl);
+      if (startpos != std::string::npos) {
+        for (int y = 0; y < cl.size(); ++y) {
+          mat[y+startpos][i] = true;
+        }
+      }
+      startpos = reversed.find(cl);
+      if (startpos != std::string::npos) {
+        for (int y = 0; y < cl.size(); ++y) {
+          mat[size - startpos - y - 1][i] = true;
+        }
+      }
+    }
+    // verticalR.push_back(cur);
+  }
 
+  vector<string> diags;
+  vector<string> diagsR;
+  // 0 to size (l->r)
+  for (int i = 0; i < size; ++i) {
+    int x = i;
+    int y = 0;
+    string cur = "";
+    while (x < size && y < size) {
+      cur += rows[y][x];
+      x++;
+      y++;
+    }
+    diags.push_back(cur);
+    reverse(cur.begin(), cur.end());
+    diagsR.push_back(cur);
+  }
+  for (int i = 1; i < size; ++i) {
+    int y = i;
+    int x = 0;
+    string cur = "";
+    while (x < size && y < size) {
+      cur += rows[y][x];
+      x++;
+      y++;
+    }
+    diags.push_back(cur);
+    reverse(cur.begin(), cur.end());
+    diagsR.push_back(cur);
+  }
 
-    // Write an answer using cout. DON'T FORGET THE "<< endl"
-    // To debug: cerr << "Debug messages..." << endl;
+  vector<string> rdiags;
+  vector<string> rdiagsR;
 
-
+  for (int i = 0; i < size; ++i) {
+    string cur = "";
+    for (int j = 0; j < size; ++j) {
+      cur += (mat[i][j]) ? string(1, rows[i][j]) : " ";
+    }
+    cout << cur << endl;
+  }
 }
