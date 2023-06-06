5c2f70a6f80709b0d646901a5763e593e57c9aae
Raskolnikov <jerry.tann@endress.com>
2023-06-06 10:42:27 +0800

    ✨ 03's winner

---
+++ CoC/03winner.py
@@ -0,0 +1,9 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+s = input()
+
+print(s.replace(':slight_smile:', ':)').replace(':disappointed:', ':(').replace(':loud_laugh:', 'XD').replace(':open_mouth:', ':o').replace(':stuck_out_tongue:', ':p'))
