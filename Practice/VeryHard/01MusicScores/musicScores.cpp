36da71dfb0044cdc7e2636644da8f2e0d28f10a3
Raskolnikov <jerry.tann@endress.com>
2023-06-21 20:36:08 +0800

    ✨ music scores, init

---
+++ Practice/VeryHard/01MusicScores/musicScores.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int w;
+    int h;
+    cin >> w >> h; cin.ignore();
+    string image;
+    getline(cin, image);
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "AQ DH" << endl;
+}
