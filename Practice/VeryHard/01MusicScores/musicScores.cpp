561330176a7e26e07da1cad53d63fc04952e4bb1
Raskolnikov <jerry.tann@endress.com>
2023-06-21 21:00:43 +0800

    🚧  where to start, where to start

--- Practice/VeryHard/01MusicScores/musicScores.cpp
+++ Practice/VeryHard/01MusicScores/musicScores.cpp
@@ -5,11 +5,6 @@
 
 using namespace std;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
 int main()
 {
     int w;
@@ -18,8 +13,6 @@ int main()
     string image;
     getline(cin, image);
 
-    // Write an answer using cout. DON'T FORGET THE "<< endl"
-    // To debug: cerr << "Debug messages..." << endl;
 
     cout << "AQ DH" << endl;
 }
