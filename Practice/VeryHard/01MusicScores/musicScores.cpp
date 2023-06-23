0c6d8c727eb0fe0c2e62610c00557e59404a943e
Raskolnikov <jerry.tann@endress.com>
2023-06-23 21:44:45 +0800

    🚧 prep lookup note

--- Practice/VeryHard/01MusicScores/musicScores.cpp
+++ Practice/VeryHard/01MusicScores/musicScores.cpp
@@ -1,17 +1,17 @@
 #include <algorithm>
 #include <cstring>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
 
 using namespace std;
 
 vector<string> split(string str, string separator) {
   vector<string> ret;
-      char* mutableStr = new char[str.size() + 1];
-    std::strcpy(mutableStr, str.data());
-  char *tok = strtok(mutableStr, separator.c_str());
+  char* mutableStr = new char[str.size() + 1];
+  std::strcpy(mutableStr, str.data());
+  char* tok = strtok(mutableStr, separator.c_str());
   while (tok != nullptr) {
     ret.push_back(tok);
     tok = strtok(nullptr, separator.c_str());
@@ -19,7 +19,7 @@ vector<string> split(string str, string separator) {
   return ret;
 }
 
-bool customCompare(pair<int,int> a, pair<int,int> b) {
+bool customCompare(pair<int, int> a, pair<int, int> b) {
   if (a.first > b.first) return true;
 
   if (a.first < b.first) return false;
@@ -27,7 +27,7 @@ bool customCompare(pair<int,int> a, pair<int,int> b) {
   return (a.second < b.second);
 }
 
-bool compare2(pair<int,int> a, pair<int,int> b) {
+bool compare2(pair<int, int> a, pair<int, int> b) {
   return a.second < b.second;
 }
 
@@ -46,8 +46,8 @@ int main() {
   bool write = false;
   int cr = 0;
   int cc = 0;
-  vector<pair<int,int>> rf(h,{0,0});
-  vector<pair<int,int>> cf(w,{0,0});
+  vector<pair<int, int>> rf(h, {0, 0});
+  vector<pair<int, int>> cf(w, {0, 0});
   for (string s : spl) {
     if (s == "W") {
       write = false;
@@ -77,27 +77,42 @@ int main() {
   }
   std::sort(rf.begin(), rf.end(), customCompare);
   std::sort(cf.begin(), cf.end(), customCompare);
-  //vector<pair<int,int>> ledgers(rf.begin(), rf.begin()+5);
-  //cerr << "ledgers " << endl;
-  // for (auto [i,idx]: ledgers) {
-  //   cerr << "i is " << i << " idx is " << idx << endl;
-  // }
+  // vector<pair<int,int>> ledgers(rf.begin(), rf.begin()+5);
+  // cerr << "ledgers " << endl;
+  //  for (auto [i,idx]: ledgers) {
+  //    cerr << "i is " << i << " idx is " << idx << endl;
+  //  }
   float sum5 = 0;
   for (int i = 0; i < 5; ++i) {
-    sum5+=rf[i].first;
+    sum5 += rf[i].first;
   }
-  float avg = sum5/5.0;
+  float avg = sum5 / 5.0;
   int totalLedgers = 5;
   float half = avg * .5;
   for (int i = 5; i < h; ++i) {
     if (rf[i].first < half) break;
 
-    totalLedgers=i+1;
+    totalLedgers = i + 1;
   }
 
-  vector<pair<int,int>> ledgers(rf.begin(), rf.begin()+totalLedgers);
+  vector<pair<int, int>> ledgers(rf.begin(), rf.begin() + totalLedgers);
+
   sort(ledgers.begin(), ledgers.end(), compare2);
-  for (auto [a,b]: ledgers) {
+
+  vector<vector<int>> ledgerLookup(5, vector<int>(2));
+  int perL = ledgers.size() / 5;
+  for (int i = 0; i < 5; ++i) {
+    ledgerLookup[i][0] = ledgers[i * perL].second;
+    ledgerLookup[i][1] = ledgerLookup[i][0] + perL - 1;
+  }
+  cerr << "ledgerLookup" << endl;
+
+  for (auto a: ledgerLookup) {
+    for (auto b: a) {
+      cerr << "min " << b[0] << " max " << b[1] << endl;
+    }
+  }
+  for (auto [a, b] : ledgers) {
     cerr << "ledger " << a << ", " << b << endl;
   }
   // cerr << "rows " << endl;
