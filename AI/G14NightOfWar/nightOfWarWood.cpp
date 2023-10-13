22a410a38ee93aecaf6908758d0faf6796f8f251
Raskolnikov <jerry.tann@endress.com>
2023-10-13 16:00:22 +0800

    ✨ new ai game

---
+++ AI/G14NightOfWar/nightOfWarWood.cpp
@@ -0,0 +1,247 @@
+/*
+
+ 	The Goal
+The goal is to finish the opponent's army or having more bucks at the end of game.
+ 	Rules
+In this game you have to control your army 1-3 soldiers depending upon the league. To understand game mechanics you need to understand few things given below.
+
+
+SOLDIER
+A Soldier is movable unit which is defined by 6 integers :-
+ownerId - The ID of player who owns this soldier.
+x - The position X of this soldier.
+y - The position Y of this soldier.
+soldierId - The unique ID of soldier.
+level - Ignore for this league.
+direction - The Direction soldier is facing 0 = UP, 1 = LEFT , 2 = DOWN, 3 = RIGHT.
+A Soldier can perform MOVE or ATTACK tasks.
+Note - the level of soldier remains 0 when game starts and can upgrade upto level 10
+
+BLOCK
+A Block is a single cell in the map defined by 3 integers :-
+blockOwner - The ID of player who owns this block.
+x - The position X of this block.
+y - The position Y of this block.
+It gives you 2 Bucks for eack Block you owned every turn.
+
+SOLDIER MOVABLE SITES
+Soldier movable sites are blocks where a soldier can move which based on direction where is soldier facing, for example: If soldier is facing UP then soldier can't move DOWN and can move UP, RIGHT and LEFT.
+
+Suppose in this picture the soldier is facing UP side, Then it can move on GREEN coloured tiles and can't move on RED coloured tiles.
+So, basically the soldier just can't move in the opposite direction of where it is facing.
+
+SOLDIER ATTACKING SITES
+Soldier attacking sites are blocks on which a soldier can attack which is again based on the direction soldier is facing. A soldier can attack on the blocks which are in range of distance 2 from soldier and are not in back side of direction.
+
+Suppose in this picture the soldier is facing UP, then GREEN coloured tiles are soldier attacking sites.
+
+And suppose in this picture the soldier is facing DOWN, then GREEN coloured tiles are soldier attacking sites.
+
+
+ACTIONS:-
+
+WAIT
+You can use this command to do nothing. The action looks like - 'WAIT' your income won't be affected by this.
+
+MOVE
+You can use this command to move your soldiers but remember a soldier can only move at SOLDIER MOVABLE SITES(Explained above).The command for the action looks like - 'MOVE ID DIRECTION' In which ID is the unique identifier of soldier and DIRECTION can be any of UP DOWN LEFT RIGHT.
+
+ATTACK
+You can use this command to attack on any opponent soldier which is in range of 2 tiles which kills the soldier and it should be one of the SOLDIER ATTACKING SITES(Explained above) and the level of opponent's soldier should be less than or equal to your soldier's level. It costs you 35 Bucks to attack. The command for the action looks like - 'ATTACK ID OPPONENT_SOLDIER_ID' In which ID is the unique identifier of soldier and OPPONENT_SOLDIER_ID is the id of the opponent soldier that you want to attack.
+
+
+Victory Conditions
+All opponent's soldiers died.
+After 200 turns, you have more Bucks than your opponent.
+Lose Conditions
+You fail to provide a valid command in time.
+You provide a unrecognized command.
+If your opponent satisfies any of victory conditions.
+ 	Game Input
+Initial input
+int MyID - Your unique player Id which could be 0 or 1.
+int MapSize - The size of board, Map = MapSize*MapSize.
+Input per turn
+int MyBucks - Your present money or bucks.
+int OppBucks - Opponent's present money or bucks.
+Next MapSize * MapSize lines each BLOCK in the board represented by 3 integers:-
+   int BlockOwner - The owner's player id of this block.
+
+   int x - This block's position x.
+
+   int y - This block's position y.
+
+
+int activeSoldierCount - Total no. of active soldier in the game.
+Next activeSoldierCount lines each SOLDIER reprensented by 6 integers:-
+   int ownerId - The owner's player id of this soldier.
+
+   int x - This soldier's position x.
+
+   int y - This soldier's position y.
+
+   int soldierId - The unique identifier of soldier.
+
+   int level - Level of the soldier ignore for first league.
+
+   int direction - The side where the soldier is facing.
+
+
+Output
+A single line from one of the following commands:-
+
+WAIT optional_message
+MOVE soldierID Direction optional_message
+ATTACK soldierID soldierId to attack on optional_message
+Constraints
+Your code should respond under 100 ms.
+Game will automatically end after 200 turns.
+MapSize = 4 For first league, 5 For second league, 8 For third league.
+MyId = 0 or 1.
+
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Jrke's special
+ * -Kill your enemy soldiers or Have more bucks than your enemy at end of game
+ **/
+
+const int UP = 0;
+const int LEFT = 1;
+const int DOWN = 2;
+const int RIGHT = 3;
+
+struct Point {
+  int x = 0;
+  int y = 0;
+  string hash() { return std::to_string(x) + "," + std::to_string(y); }
+  Point(int x = -1, int y = -1)
+      : x(x), y(y){};  // init list syntax Constructor followed by :
+                       // field(param), field(param)
+};
+
+struct Soldier {
+  int id = -1;
+  int owner = -1;
+  int level;
+  int direction = UP;
+  Point p;
+};
+struct Block {
+  Point p;
+  int owner = 0;
+};
+class Game {
+  int myId = 0;
+  int mapSize = 0;
+
+  vector<vector<Block>> map;
+  vector<Soldier> my_s;
+  vector<Soldier> e_s;
+
+ public:
+  Game(int myId = 0, int mapSize = 0) : myId(myId), mapSize(mapSize){};
+
+  void reset() {
+    map.clear();
+    my_s.clear();
+    e_s.clear();
+  };
+
+  void addMapLine(vector<Block> line) { map.push_back(line); };
+
+  void addMySoldier(Soldier s) { my_s.push_back(s); };
+
+  void addESoldier(Soldier s) { e_s.push_back(s); }
+
+  void debug() {
+    cerr << "printing game map" << endl;
+    for (auto line: map) {
+      for (auto block: line) {
+        cerr << "block " << block.owner << endl;
+        cerr << block.p.hash() << endl;
+      }
+    }
+    for (auto mys: my_s) {
+      cerr << "my soldiers " << mys.id << endl;
+    }
+    for (auto es:e_s) {
+      cerr << "enemy soldiers " << es.id << endl;
+    }
+  }
+};
+
+int main() {
+  int my_id;  // Your unique player Id
+  cin >> my_id;
+  cin.ignore();
+  int map_size;  // the size of map MapSize*MapSize
+  cin >> map_size;
+  cin.ignore();
+  Game g = Game(my_id, map_size);
+
+  // game loop
+  while (1) {
+    g.reset();
+    int my_bucks;  // Your Money
+    cin >> my_bucks;
+    cin.ignore();
+    int opp_bucks;  // Opponent Money
+    cin >> opp_bucks;
+    cin.ignore();
+    cerr << "my bucks " << my_bucks << endl;
+    cerr << "opp bucks " << opp_bucks << endl;
+    for (int i = 0; i < map_size; i++) {
+      vector<Block> line;
+      for (int j = 0; j < map_size; j++) {
+        int block_owner;  // The playerId of this box owned player
+        int x;            // This block's position x
+        int y;            // This block's position y
+        Block b;
+        cin >> block_owner >> x >> y;
+        b.owner = block_owner;
+        Point p = Point(x, y);
+        b.p = p;
+        line.push_back(b);
+        cin.ignore();
+      }
+      g.addMapLine(line);
+    }
+    int active_soldier_count;  // Total no. of active soldier in the game
+    cin >> active_soldier_count;
+    cin.ignore();
+    for (int i = 0; i < active_soldier_count; i++) {
+      int owner_id;    // owner of the soldier
+      int x;           // This soldier's position x
+      int y;           // This soldier's position y
+      int soldier_id;  // The unique identifier of soldier
+      int level;       // Level of the soldier ignore for first league
+      int direction;  // The side where the soldier is facing 0 = UP, 1 = LEFT ,
+                      // 2 = DOWN, 3 = RIGHT
+      cin >> owner_id >> x >> y >> soldier_id >> level >> direction;
+      bool me = owner_id == my_id;
+      Soldier s;
+      s.owner = owner_id;
+      s.p = Point(x,y);
+      s.level = level;
+      s.direction = direction;
+      s.id = soldier_id;
+      if (me) { g.addMySoldier(s); } else { g.addESoldier(s); }
+
+      cin.ignore();
+    }
+
+    // print any of actions - WAIT | MOVE <soldierId> <direction> | ATTACK
+    // <soldierID> <soldierId to attack on> | LATER > UPGRADE <id> | DEGRADE
+    // <opponent id> | SUICIDE <id>
+    g.debug();
+    cout << "WAIT" << endl;
+  }
+}
