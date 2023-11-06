8571e8ca1e92b2921abb946f7961b0b6b4dafb47
Raskolnikov <jerry.tann@endress.com>
2023-11-06 16:17:22 +0800

    🚧 better than nothing

--- AI/G14-TicTacToe/ticTacToeWood.cpp
+++ AI/G14-TicTacToe/ticTacToeWood.cpp
@@ -1,34 +1,41 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+struct Game {
+  vector<vector<bool>> _opp;
+  vector<vector<bool>> _mine;
+  Game() {
+    _opp.resize(3, vector<bool>(3, false));
+    _mine.resize(3, vector<bool>(3, false));
+  }
 
-int main()
-{
+  void move(bool opp, int r, int c) {
+    auto v = opp ? _opp : _mine;
+    v[r][c] = true;
+  }
+};
 
-    // game loop
-    while (1) {
-        int opponent_row;
-        int opponent_col;
-        cin >> opponent_row >> opponent_col; cin.ignore();
-        int valid_action_count;
-        cin >> valid_action_count; cin.ignore();
-        for (int i = 0; i < valid_action_count; i++) {
-            int row;
-            int col;
-            cin >> row >> col; cin.ignore();
-        }
-
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
-
-        cout << "0 0" << endl;
+int main() {
+  // game loop
+  while (1) {
+    int opponent_row;
+    int opponent_col;
+    cin >> opponent_row >> opponent_col;
+    cin.ignore();
+    int valid_action_count;
+    cin >> valid_action_count;
+    cin.ignore();
+    for (int i = 0; i < valid_action_count; i++) {
+      int row;
+      int col;
+      cin >> row >> col;
+      cin.ignore();
     }
+
+    cout << "0 0" << endl;
+  }
 }
